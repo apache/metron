4188dc72dc24d5b052c2b52cdf9ee8a131c83bfd
Nick Allen <nick@nickallen.org>
2016-01-27 18:20:24 -0500

    Snort topology successfully shoveling messages to elasticsearch

--- metron-streaming/Metron-MessageParsers/src/main/java/org/apache/metron/parsing/parsers/BasicSnortParser.java
+++ metron-streaming/Metron-MessageParsers/src/main/java/org/apache/metron/parsing/parsers/BasicSnortParser.java
@@ -1,8 +1,13 @@
 package org.apache.metron.parsing.parsers;
 
-import org.apache.hadoop.mapreduce.lib.input.InvalidInputException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.apache.metron.parser.interfaces.MessageParser;
 import org.json.simple.JSONObject;
+
+import java.text.ParseException;
 
 @SuppressWarnings("serial")
 public class BasicSnortParser extends AbstractParser implements MessageParser {
@@ -10,7 +15,7 @@ public class BasicSnortParser extends AbstractParser implements MessageParser {
 	/**
 	 * The default field names for Snort Alerts.
 	 */
-	static final String[] fieldNames = new String[] {
+	private String[] fieldNames = new String[] {
 			"timestamp",
 			"sig_generator",
 			"sig_id",
@@ -43,7 +48,7 @@ public class BasicSnortParser extends AbstractParser implements MessageParser {
 	/**
 	 * Snort alerts are received as CSV records
 	 */
-	static final String recordDelimiter = ",";
+	private String recordDelimiter = ",";
 
 	@Override
 	public JSONObject parse(byte[] rawMessage) {
@@ -52,19 +57,31 @@ public class BasicSnortParser extends AbstractParser implements MessageParser {
 		try {
 			// snort alerts expected as csv records
 			String csvMessage = new String(rawMessage, "UTF-8");
-			String[] records = csvMessage.split(recordDelimiter);
+			String[] records = csvMessage.split(recordDelimiter, -1);
 
 			// validate the number of fields
 			if (records.length != fieldNames.length) {
 				throw new IllegalArgumentException("Unexpected number of fields, expected: " + fieldNames.length + " got: " + records.length);
 			}
 
-			// TODO convert timestamp to epoch?
-
 			// build the json record from each field
 			for (int i=0; i<records.length; i++) {
-				jsonMessage.put(fieldNames[i], records[i]);
+			
+				String field = fieldNames[i];
+				String record = records[i];
+				
+				if("timestamp".equals(field)) {
+
+					// convert the timestamp to epoch
+					jsonMessage.put("timestamp", toEpoch(record));
+					
+				} else {
+					jsonMessage.put(field, record);
+				}
 			}
+
+			// add original msg; required by 'checkForSchemaCorrectness'
+			jsonMessage.put("original_string", csvMessage);
 
 		} catch (Exception e) {
 
@@ -75,4 +92,43 @@ public class BasicSnortParser extends AbstractParser implements MessageParser {
 
 		return jsonMessage;
 	}
+
+	/**
+	 * Parses Snort's default date-time representation and
+	 * converts to epoch.
+	 * @param datetime Snort's default date-time as String '01/27-16:01:04.877970'
+	 * @return epoch time
+	 * @throws java.text.ParseException 
+	 */
+	private long toEpoch(String snortDatetime) throws ParseException {
+		
+		/*
+		 * TODO how does Snort not embed the year in their default timestamp?! need to change this in 
+		 * Snort configuration.  for now, just assume current year.
+		 */
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		String withYear = Integer.toString(year) + " " + snortDatetime;
+		
+		// convert to epoch time
+		SimpleDateFormat df = new SimpleDateFormat("yyyy MM/dd-HH:mm:ss.S");
+		Date date = df.parse(withYear);
+		return date.getTime();
+	}
+
+	public String getRecordDelimiter() {
+		return this.recordDelimiter;
+	}
+
+	public void setRecordDelimiter(String recordDelimiter) {
+		this.recordDelimiter = recordDelimiter;
+	}
+
+	public String[] getFieldNames() {
+		return this.fieldNames;
+	}
+
+	public void setFieldNames(String[] fieldNames) {
+		this.fieldNames = fieldNames;
+	}
+
 }
