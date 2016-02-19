e759672b8bbca64bb82e254ea6332e2adee98cce
nickwallen <nickwallen@gmail.com>
2016-02-19 14:14:59 -0500

    METRON-39 Replay PCAP Data closes apache/incubator-metron#25

--- deployment/roles/yaf/files/start-yaf.sh
+++
@@ -1,1 +0,0 @@
-/usr/local/bin/yaf --in eth0 --live pcap --idle-timeout 0 | /usr/local/bin/yafscii --tabular --print-header | /usr/hdp/current/kafka-broker/bin/kafka-console-producer.sh --broker-list $1 --topic ipfix
