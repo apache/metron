7745d8d29cc7d001409d58a069dda8af47c4a9df
JonathanRider <jonathan.rider@capitalone.com>
2016-05-17 10:45:29 -0400

    [METRON-158] Fixed timestamp issue with non-standard timestamps

--- metron-platform/metron-parsers/src/main/java/org/apache/metron/parsers/mcafeeepo/McAfeeEpoParser.java
+++ metron-platform/metron-parsers/src/main/java/org/apache/metron/parsers/mcafeeepo/McAfeeEpoParser.java
@@ -1,28 +1,34 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.metron.parsers.mcafeeepo;
 
 
-        import com.opencsv.CSVReader;
+import org.apache.metron.parsers.BasicParser;
+import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-        import oi.thekraken.grok.api.exception.GrokException;
-        import org.apache.metron.parsers.BasicParser;
-        import org.apache.metron.parsers.GrokParser;
-        import org.apache.metron.parsers.bluecoat.BasicBluecoatParser;
-        import org.json.simple.JSONObject;
-        import org.slf4j.Logger;
-        import org.slf4j.LoggerFactory;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
 
-        import java.io.IOException;
-        import java.io.StringReader;
-        import java.io.UnsupportedEncodingException;
-        import java.text.ParseException;
-        import java.text.SimpleDateFormat;
-        import java.util.*;
-        import java.util.regex.Matcher;
-        import java.util.regex.Pattern;
-
-/**
- * Created by bnp617 on 4/26/2016.
- */
 public class McAfeeEpoParser extends BasicParser {
     private static final Logger _LOG = LoggerFactory.getLogger(McAfeeEpoParser.class);
     private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
@@ -56,6 +62,8 @@ public class McAfeeEpoParser extends BasicParser {
 
                 if(keypair[0].equals("timestamp")){
                     String timestamp = keypair[1];
+                    int missingZeros = "yyyy-MM-dd HH:mm:ss.SSS".length() - timestamp.length();
+                    timestamp += new String(new char[missingZeros]).replace("\0", "0"); // add on the missing zeros
                     payload.put(keypair[0], df.parse(timestamp).getTime());
                 } else if(!keypair[1].equals("NULL") && !keypair[1].equals("_")){
                     payload.put(keypair[0], keypair[1]);
--- metron-platform/metron-parsers/src/test/java/org/apache/metron/parsers/mcafeeepo/BasicMcAfeeEpoTest.java
+++ metron-platform/metron-parsers/src/test/java/org/apache/metron/parsers/mcafeeepo/BasicMcAfeeEpoTest.java
@@ -1,16 +1,30 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.metron.parsers.mcafeeepo;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.List;
-
-import org.apache.metron.parsers.bluecoat.BasicBluecoatParser;
 import org.json.simple.JSONObject;
 import org.junit.Test;
 
-/**
- * Created by bnp617 on 5/16/16.
- */
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class BasicMcAfeeEpoTest {
 
@@ -23,7 +37,7 @@ public class BasicMcAfeeEpoTest {
 
     @Test
     public void test1() {
-        String testString = "<13> computer.website.com \"2016-04-11 14:20:15\" timestamp=\"2016-04-11 14:20:15.693\", AutoID=\"136424372\", signature=\"WRITE_DENIED\", threat_type=\"none\", signature_id=\"20719\", category=\"cc.file.block\", severity_id=\"3\", event_description=\"File Write Denied\", detected_timestamp=\"2016-04-11 13:29:09.0\", file_name=\"c:\\windows\\system32\\folder\\file\", detection_method=\"NULL\", vendor_action=\"deny write\", threat_handled=\"1\", logon_user=\"NT AUTHORITY\\NETWORK SERVICE\", user=\"abc123\", dest_nt_domain=\"APL\", dest_dns=\"IMCAVA12345\", dest_nt_host=\"IMCAVA12345\", fqdn=\"IMCAVA12345.something.website.com\", dest_ip=\"100.170.200.100\", dest_mac=\"000000000000\", os=\"Windows 7\", sp=\"Service Pack 1\", os_version=\"6.1\", os_build=\"7601\", timezone=\"Eastern Standard Time\", src_dns=\"NULL\", src_ip=\"200.23.55.70\", src_mac=\"NULL\", process=\"NULL\", url=\"NULL\", source_logon_user=\"NULL\", is_laptop=\"1\", product=\"Solidifier\", product_version=\"6.1.3.436\", engine_version=\"NULL\", dat_version=\"NULL\", vse_dat_version=\"8130.0000\", vse_engine64_version=\"5800.7501\", vse_engine_version=\"5800.7501\", vse_hotfix=\"5\", vse_product_version=\"8.8.0.1385\"";
+        String testString = "<13> computer.website.com \"2016-04-11 14:20:15\" timestamp=\"2016-04-11 14:20:15.69\", AutoID=\"136424372\", signature=\"WRITE_DENIED\", threat_type=\"none\", signature_id=\"20719\", category=\"cc.file.block\", severity_id=\"3\", event_description=\"File Write Denied\", detected_timestamp=\"2016-04-11 13:29:09.0\", file_name=\"c:\\windows\\system32\\folder\\file\", detection_method=\"NULL\", vendor_action=\"deny write\", threat_handled=\"1\", logon_user=\"NT AUTHORITY\\NETWORK SERVICE\", user=\"abc123\", dest_nt_domain=\"APL\", dest_dns=\"IMCAVA12345\", dest_nt_host=\"IMCAVA12345\", fqdn=\"IMCAVA12345.something.website.com\", dest_ip=\"100.170.200.100\", dest_mac=\"000000000000\", os=\"Windows 7\", sp=\"Service Pack 1\", os_version=\"6.1\", os_build=\"7601\", timezone=\"Eastern Standard Time\", src_dns=\"NULL\", src_ip=\"200.23.55.70\", src_mac=\"NULL\", process=\"NULL\", url=\"NULL\", source_logon_user=\"NULL\", is_laptop=\"1\", product=\"Solidifier\", product_version=\"6.1.3.436\", engine_version=\"NULL\", dat_version=\"NULL\", vse_dat_version=\"8130.0000\", vse_engine64_version=\"5800.7501\", vse_engine_version=\"5800.7501\", vse_hotfix=\"5\", vse_product_version=\"8.8.0.1385\"";
 
         List<JSONObject> result = bmap.parse(testString.getBytes());
 
@@ -34,7 +48,7 @@ public class BasicMcAfeeEpoTest {
         assertEquals(jo.get("priority"), "13");
 
 
-        assertEquals(jo.get("timestamp"), 1460384415693L);
+        assertEquals(jo.get("timestamp"), 1460384415690L);
         assertEquals(jo.get("AutoID"), "136424372");
         assertEquals(jo.get("signature"), "WRITE_DENIED");
         assertEquals(jo.get("threat_type"), "none");
