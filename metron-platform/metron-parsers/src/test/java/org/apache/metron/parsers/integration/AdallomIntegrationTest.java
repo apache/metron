319fb2b3d2d2ac8cb669eef2176984f66a6c4cd9
DomenicPuzio <domenic.puzio@captitalone.com>
2016-05-18 15:02:21 -0400

    Adding topology configs and test for Adallom CEF data source

---
+++ metron-platform/metron-parsers/src/test/java/org/apache/metron/parsers/integration/AdallomIntegrationTest.java
@@ -0,0 +1,50 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.metron.parsers.integration;
+
+import org.apache.metron.TestConstants;
+
+public class AdallomIntegrationTest extends ParserIntegrationTest {
+	
+	  @Override
+	  public String getFluxPath() {
+	    return "./src/main/flux/adallom/test.yaml";
+	  }
+
+	  @Override
+	  public String getSampleInputPath() {
+	    return TestConstants.SAMPLE_DATA_INPUT_PATH + "AdallomOutput";
+	  }
+
+	  @Override
+	  public String getSampleParsedPath() {
+	    return TestConstants.SAMPLE_DATA_PARSED_PATH + "AdallomParsed";
+	  }
+
+	  @Override
+	  public String getSensorType() {
+	    return "adallom";
+	  }
+
+	  @Override
+	  public String getFluxTopicProperty() {
+	    return "spout.kafka.topic.adallom";
+	  }
+
+}
