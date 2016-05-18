7b257620c20246a9670f6f39dc7c474bcff152d2
MorganEmery <Morgan.Emery@capitalone.com>
2016-05-18 14:10:56 -0400

    added Big IP parser

---
+++ metron-platform/metron-parsers/src/main/java/org/apache/metron/parsers/bigip/GrokBigIpParser.java
@@ -0,0 +1,118 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.metron.parsers.bigip;
+
+import oi.thekraken.grok.api.Match;
+import oi.thekraken.grok.api.exception.GrokException;
+import org.apache.metron.parsers.GrokParser;
+import org.json.simple.JSONObject;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GrokBigIpParser extends GrokParser {
+
+	public GrokBigIpParser(String grokHdfsPath, String patternLabel) {
+		super(grokHdfsPath, patternLabel);
+	}
+
+	private final static String BIGIP_NEWSESSION_PATTERN = "NEWSESSION";
+	private final static String BIGIP_STATISTICS_PATTERN = "STATISTICS";
+	private final static String BIGIP_LOGIN_PATTERN = "LOGIN";
+	private final static String BIGIP_ACCESSPOLICYRESULT_PATTERN = "ACCESSPOLICYRESULT";
+	private final static String BIGIP_SESSION_PATTERN = "SESSION";
+	private final static String BIGIP_SYSTEM_PATTERN = "SYSTEM";
+
+	@Override
+	protected long formatTimestamp(Object value) {
+		long epochTimestamp = System.currentTimeMillis();
+		if (value != null) {
+			try {
+				epochTimestamp = toEpoch(Calendar.getInstance().get(Calendar.YEAR)  + " " + value);
+			} catch (ParseException e) {
+				//default to current time
+			}
+		}
+		return epochTimestamp;
+	}
+
+	@Override
+	protected void postParse(JSONObject message) {
+		removeEmptyFields(message);
+		message.remove("timestamp_string");
+		if (message.containsKey("message")) {
+			String messageValue = (String) message.get("message");
+			if (messageValue.contains("New session")) {
+				parseMessage(message, "session", BIGIP_NEWSESSION_PATTERN);
+			}
+			else if (messageValue.contains("session.logon")) {
+				parseMessage(message, "login", BIGIP_LOGIN_PATTERN);
+			}
+			else if (messageValue.contains("Session statistics")) {
+				parseMessage(message, "statistics", BIGIP_STATISTICS_PATTERN);
+			}
+			else if (messageValue.contains("Access policy result")) {
+				parseMessage(message, "access policy result", BIGIP_ACCESSPOLICYRESULT_PATTERN);
+			}
+			else {
+				Pattern pattern = Pattern.compile("\\d{8}:5: \\w{8}:");
+				Matcher matcher = pattern.matcher(messageValue);
+				// Other session messsages match this regex (contain a log code and session id)
+				if (matcher.find()){
+					parseMessage(message, "session", BIGIP_SESSION_PATTERN);
+				}
+				// System messages do not contain a log code and session id
+				else{
+					parseMessage(message, "system", BIGIP_SYSTEM_PATTERN);
+				}
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void removeEmptyFields(JSONObject json) {
+		Iterator<Object> keyIter = json.keySet().iterator();
+		while (keyIter.hasNext()) {
+			Object key = keyIter.next();
+			Object value = json.get(key);
+			if (null == value || "".equals(value.toString())) {
+				keyIter.remove();
+			}
+		}
+	}
+
+	//Extracts the appropriate fields from login messages
+	@SuppressWarnings("unchecked")
+	private void parseMessage(JSONObject json, String messageType, String pattern) {
+		json.put("big_ip_message_type", messageType);
+		try {
+			grok.compile("%{" + pattern + "}");
+			Match gm = grok.match((String) json.get("message"));
+			gm.captures();
+			json.putAll(gm.toMap());
+		} catch (GrokException e) {
+			LOG.debug("Error parsing Big IP log", e.toString());
+		}
+		json.remove(pattern);
+	}
+
+}
