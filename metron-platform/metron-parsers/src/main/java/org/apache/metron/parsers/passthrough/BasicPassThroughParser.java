74ec3a370bb7abecfb6c2f0ddfedadd43f6209e7
Chris Gorski <cgorski007@gmail.com>
2016-05-23 12:26:31 -0700

    Add pass-through parser and sensor configs for all data sources

---
+++ metron-platform/metron-parsers/src/main/java/org/apache/metron/parsers/passthrough/BasicPassThroughParser.java
@@ -0,0 +1,67 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.metron.parsers.passthrough;
+
+import org.apache.metron.parsers.BasicParser;
+import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@SuppressWarnings("serial")
+public class BasicPassThroughParser extends BasicParser {
+
+	private static final Logger _LOG = LoggerFactory.getLogger(BasicPassThroughParser.class);
+	
+	@Override
+	public void init() {
+
+	}
+
+	@SuppressWarnings({ "unchecked", "unused" })
+	public List<JSONObject> parse(byte[] msg) {
+
+		String message = "";
+		List<JSONObject> messages = new ArrayList<>();
+		JSONObject payload = new JSONObject();
+		
+		try {
+			message = new String(msg, "UTF-8");
+
+			payload.put("original_string", message);
+            payload.put("is_parsed", false);
+
+			Date date = new Date();
+		    long epoch = date.getTime();
+		    	    
+			payload.put("timestamp", epoch);
+			
+			messages.add(payload);
+			return messages;
+		} catch (Exception e) {
+			e.printStackTrace();
+			_LOG.error("Failed to parse: " + message);
+			return null;
+		}
+	}
+
+}
