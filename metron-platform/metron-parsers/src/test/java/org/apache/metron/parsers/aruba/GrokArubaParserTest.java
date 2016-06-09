5754eac1b5b5db182e5b8f37abbae95366bad993
Phil Austin <philip.austin@capitalone.com>
2016-06-09 16:36:32 -0400

    [METRON-164] revamped Aruba parser

--- metron-platform/metron-parsers/src/test/java/org/apache/metron/parsers/aruba/GrokArubaParserTest.java
+++ metron-platform/metron-parsers/src/test/java/org/apache/metron/parsers/aruba/GrokArubaParserTest.java
@@ -50,17 +50,20 @@ public class GrokArubaParserTest {
 		String testString = "<143>2016-04-29 04:27:31,823  110.137.3.2 CPPM_Session_Detail 473964876 1 0 "
 				+ "id=5983679670,session_id=R011cf48e-04-57231aa3,type=RADIUS_IN,attr_name=Radius:IETF:User-Name,"
 				+ "attr_value=ac81125d02ea,timestamp=2016-04-29 04:26:13.002367-04";
+		System.out.println(testString);
 		List<JSONObject> result = parser.parse(testString.getBytes());
 		JSONObject parsedJSON = result.get(0);
+		System.out.println(parsedJSON);
 
     Assert.assertEquals(parsedJSON.get("priority"), 143);
 		Assert.assertEquals(parsedJSON.get("ip_src_addr"), "110.137.3.2");
-		Assert.assertEquals(parsedJSON.get("message_hex"), 473964876);
-		Assert.assertEquals(parsedJSON.get("message_type"), "CPPM_Session_Detail");
-		Assert.assertEquals(parsedJSON.get("timestamp") + "", "1461904051000");
+		Assert.assertEquals(parsedJSON.get("category"), "CPPM_Session_Detail");
+		Assert.assertEquals(parsedJSON.get("message_id"), 473964876);
+		Assert.assertEquals(parsedJSON.get("timestamp") , 1461904051000L);
 		Assert.assertEquals(parsedJSON.get("attr_name"), "Radius:IETF:User-Name");
 		Assert.assertEquals(parsedJSON.get("session_id"), "R011cf48e-04-57231aa3");
-		Assert.assertEquals(parsedJSON.get("message_id"), "5983679670");
+		Assert.assertEquals(parsedJSON.get("message_id"), 473964876);
+		Assert.assertEquals(parsedJSON.get("id"), "5983679670");
 		Assert.assertEquals(parsedJSON.get("type"), "RADIUS_IN");
 		Assert.assertEquals(parsedJSON.get("request_timestamp"), "2016-04-29 04:26:13.002367-04");
   }
@@ -91,6 +94,19 @@ public class GrokArubaParserTest {
 		
 		//Result should be null due to malformed message
 		Assert.assertEquals(result, null);
+	}
+
+	@Test
+	public void testPrint() throws Exception {
+		GrokArubaParser parser = new GrokArubaParser();
+		parser.configure(parserConfig);
+		String[] lines = { "<143>2016-04-29 04:27:31,817  120.44.205.9 CPPM_Session_Detail 473963971 1 0 id=5983678767,session_id=R011cf475-04-57231a94,type=AUTH_IN,attr_name=Authorization:[Insight Repository]:Minutes-Since-Auth,attr_value=82775,timestamp=2016-04-29 04:25:56.77251-04",
+				"<143>2016-04-29 04:27:31,819  10.148.5.141 CPPM_Session_Detail 473964265 1 0 id=5983679060,session_id=R011cf47d-04-57231a9b,type=INTERNAL_IN,attr_name=Connection:SSID,attr_value= ,timestamp=2016-04-29 04:26:04.866339-04",
+				"<143>2016-04-29 04:27:31,823  110.137.3.2 CPPM_Session_Detail 473964876 1 0 id=5983679670,session_id=R011cf48e-04-57231aa3,type=RADIUS_IN,attr_name=Radius:IETF:User-Name,attr_value=ac81125d02ea,timestamp=2016-04-29 04:26:13.002367-04"
+				};
+		for (String line : lines) {
+			System.out.println(			parser.parse(line.getBytes()).get(0) );
+		}
 	}
 
 }
