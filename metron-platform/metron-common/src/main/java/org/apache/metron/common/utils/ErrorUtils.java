ee0eb5bbbdf7d03180fba09f7fee3fee5d4299bb
cstella <cestella@gmail.com>
2016-06-13 14:09:32 -0400

    METRON-217 The elasticsearch writer should allow multiple ES hosts to be passed in via a List. This should be backwards compatible. METRON-217 The parser topology numSpoutTasks and numParserTasks are swapped METRON-217 The grok parser should throw an exception if it cannot parse so the message that could not be parsed is sent to the error queue. This closes apache/incubator-metron#149

--- metron-platform/metron-common/src/main/java/org/apache/metron/common/utils/ErrorUtils.java
+++ metron-platform/metron-common/src/main/java/org/apache/metron/common/utils/ErrorUtils.java
@@ -17,6 +17,9 @@
  */
 package org.apache.metron.common.utils;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -60,5 +63,26 @@ public class ErrorUtils {
 		JSONObject error = ErrorUtils.generateErrorMessage(t.getMessage(), t);
 		collector.emit(errorStream, new Values(error));
 		collector.reportError(t);
+	}
+
+	public static String generateThreadDump() {
+		final StringBuilder dump = new StringBuilder();
+		final ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+		final ThreadInfo[] threadInfos = threadMXBean.getThreadInfo(threadMXBean.getAllThreadIds(), 100);
+		for (ThreadInfo threadInfo : threadInfos) {
+			dump.append('"');
+			dump.append(threadInfo.getThreadName());
+			dump.append("\" ");
+			final Thread.State state = threadInfo.getThreadState();
+			dump.append("\n   java.lang.Thread.State: ");
+			dump.append(state);
+			final StackTraceElement[] stackTraceElements = threadInfo.getStackTrace();
+			for (final StackTraceElement stackTraceElement : stackTraceElements) {
+				dump.append("\n        at ");
+				dump.append(stackTraceElement);
+			}
+			dump.append("\n\n");
+		}
+		return dump.toString();
 	}
 }
