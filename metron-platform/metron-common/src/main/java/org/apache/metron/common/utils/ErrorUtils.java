0c6b09bae814dc9951278332d1760e91c2ac8548
mmiklavc <michael.miklavcic@gmail.com>
2016-06-13 17:34:15 -0400

    METRON-203 General best practice and bug fixes (mmiklavcic via cestella) closes apache/incubator-metron#146

--- metron-platform/metron-common/src/main/java/org/apache/metron/common/utils/ErrorUtils.java
+++ metron-platform/metron-common/src/main/java/org/apache/metron/common/utils/ErrorUtils.java
@@ -17,39 +17,98 @@
  */
 package org.apache.metron.common.utils;
 
+import backtype.storm.task.OutputCollector;
+import backtype.storm.tuple.Values;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.metron.common.Constants;
+import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Optional;
+import java.util.function.Function;
 
-import backtype.storm.task.OutputCollector;
-import backtype.storm.tuple.Values;
-import org.apache.commons.lang.exception.ExceptionUtils;
-import org.apache.metron.common.Constants;
-import org.json.simple.JSONObject;
+import static java.lang.String.format;
 
 public class ErrorUtils {
+  private final static Logger LOGGER = LoggerFactory.getLogger(ErrorUtils.class);
 
-	@SuppressWarnings("unchecked")
-	public static JSONObject generateErrorMessage(String message, Throwable t)
-	{
-		JSONObject error_message = new JSONObject();
-		
+  public enum RuntimeErrors {
+    ILLEGAL_ARG(t -> new IllegalArgumentException(formatReason(t), t.getRight().orElse(null))),
+    ILLEGAL_STATE(t -> new IllegalStateException(formatReason(t), t.getRight().orElse(null)));
+
+    Function<Pair<String, Optional<Throwable>>, RuntimeException> func;
+
+    RuntimeErrors(Function<Pair<String, Optional<Throwable>>, RuntimeException> func) {
+      this.func = func;
+    }
+
+    /**
+     * Throw runtime exception with "reason".
+     *
+     * @param reason Message to include in exception
+     */
+    public void throwRuntime(String reason) {
+      throwRuntime(reason, Optional.empty());
+    }
+
+    /**
+     * Throw runtime exception with format "reason + cause message + cause Throwable"
+     *
+     * @param reason Message to include in exception
+     * @param t Wrapped exception
+     */
+    public void throwRuntime(String reason, Throwable t) {
+      throwRuntime(reason, Optional.of(t));
+    }
+
+    /**
+     * Throw runtime exception with format "reason + cause message + cause Throwable".
+     * If the optional Throwable is empty/null, the exception will only include "reason".
+     *
+     * @param reason Message to include in exception
+     * @param t Optional wrapped exception
+     */
+    public void throwRuntime(String reason, Optional<Throwable> t) {
+      throw func.apply(Pair.of(reason, t));
+    }
+
+    private static String formatReason(Pair<String, Optional<Throwable>> p) {
+      return formatReason(p.getLeft(), p.getRight());
+    }
+
+    private static String formatReason(String reason, Optional<Throwable> t) {
+      if (t.isPresent()) {
+        return format("%s - reason:%s", reason, t.get());
+      } else {
+        return format("%s", reason);
+      }
+    }
+  }
+
+  @SuppressWarnings("unchecked") // JSONObject extends HashMap w/o type parameters
+  public static JSONObject generateErrorMessage(String message, Throwable t) {
+    JSONObject error_message = new JSONObject();
+
 		/*
-		 * Save full stack trace in object.
+     * Save full stack trace in object.
 		 */
 		String stackTrace = ExceptionUtils.getStackTrace(t);
 		
 		String exception = t.toString();
 		
-		error_message.put("time", System.currentTimeMillis());
-		try {
-			error_message.put("hostname", InetAddress.getLocalHost().getHostName());
-		} catch (UnknownHostException ex) {
-			// TODO Auto-generated catch block
-			ex.printStackTrace();
-		}
+    error_message.put("time", System.currentTimeMillis());
+    try {
+      error_message.put("hostname", InetAddress.getLocalHost().getHostName());
+    } catch (UnknownHostException ex) {
+      LOGGER.info("Unable to resolve hostname while generating error message", ex);
+    }
 		
 		error_message.put("message", message);
 		error_message.put(Constants.SENSOR_TYPE, "error");
