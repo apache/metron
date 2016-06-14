6551290a169b369d08f018f9e06c58eb23b39a5f
cstella <cestella@gmail.com>
2016-06-14 11:10:42 -0400

    Updating documentation for the transformation test.

--- metron-platform/metron-common/src/test/java/org/apache/metron/common/field/transformation/MTLTransformationTest.java
+++ metron-platform/metron-common/src/test/java/org/apache/metron/common/field/transformation/MTLTransformationTest.java
@@ -45,9 +45,13 @@ public class MTLTransformationTest {
    */
   @Multiline
   public static String mtlConfig;
-
+  /**
+   * Test the happy path.  This ensures that a simple transformation, converting a timestamp in a yyyy-MM-dd HH:mm:ss
+   * format can be converted to the expected UTC MS since Epoch.
+   */
   @Test
   public void testMTL() throws Exception {
+
     SensorParserConfig c = SensorParserConfig.fromBytes(Bytes.toBytes(mtlConfig));
     FieldTransformer handler = Iterables.getFirst(c.getFieldTransformations(), null);
     JSONObject input = new JSONObject(new HashMap<String, Object>() {{
@@ -58,10 +62,17 @@ public class MTLTransformationTest {
     Assert.assertEquals(expected, input.get("utc_timestamp"));
     Assert.assertTrue(input.containsKey("timestamp"));
   }
+  /**
+   * Ensures that if we try to transform with a field which does not exist, it does not
+   * 1. throw an exception
+   * 2. do any transformation.
+   */
   @Test
   public void testMTL_negative() throws Exception {
+
     SensorParserConfig c = SensorParserConfig.fromBytes(Bytes.toBytes(mtlConfig));
     FieldTransformer handler = Iterables.getFirst(c.getFieldTransformations(), null);
+    //no input fields => no transformation
     JSONObject input = new JSONObject(new HashMap<String, Object>() {{
     }});
     handler.transformAndUpdate(input, new HashMap<>());
@@ -94,14 +105,25 @@ public class MTLTransformationTest {
   @Multiline
   public static String mtlConfig_multi;
 
+  /**
+   * A more complicated test where we are transforming multiple fields:
+   * 1. Convert a timestamp field in yyyy-MM-dd HH:mm:ss format to unix epoch while
+   *    looking up the timezone based on a second field, dc, in a map being kept in the parser config.
+   *    If the data center isn't in the map, then the default is UTC
+   * 2. Extract the host from a URL field and convert to lowercase
+   * 3. Extract the protocol of the URL field
+   **/
   @Test
   public void testMTL_multi() throws Exception {
+
     SensorParserConfig c = SensorParserConfig.fromBytes(Bytes.toBytes(mtlConfig_multi));
     FieldTransformer handler = Iterables.getFirst(c.getFieldTransformations(), null);
     {
+      //We need a timestamp field, a URL field and a data center field
       JSONObject input = new JSONObject(new HashMap<String, Object>() {{
         put("timestamp", "2016-01-05 17:02:30");
         put("url", "https://caseystella.com/blog");
+        //looking up the data center in portland, which doesn't exist in the map, so we default to UTC
         put("dc", "portland");
       }});
       handler.transformAndUpdate(input, c.getParserConfig());
@@ -113,6 +135,7 @@ public class MTLTransformationTest {
       Assert.assertTrue(input.containsKey("url"));
     }
     {
+      //now we see what happens when we change the data center to london, which is in the map
       JSONObject input = new JSONObject(new HashMap<String, Object>() {{
         put("timestamp", "2016-01-05 17:02:30");
         put("url", "https://caseystella.com/blog");
@@ -126,6 +149,7 @@ public class MTLTransformationTest {
       Assert.assertTrue(input.containsKey("timestamp"));
       Assert.assertTrue(input.containsKey("url"));
     }
+    //now we ensure that because we don't have a data center field at all, it's defaulted to UTC.
     {
       JSONObject input = new JSONObject(new HashMap<String, Object>() {{
         put("timestamp", "2016-01-05 17:02:30");
