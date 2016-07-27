9c70a44c72c148f1054cafcde61e92cc0f1c2de1
justinleet <justinjleet@gmail.com>
2016-07-27 14:07:15 -0400

    METRON-319 Automate RPM Builds (justinleet via dlyle65535) closes apache/incubator-metron#199

--- metron-deployment/packaging/docker/rpm-docker/build.sh
+++ metron-deployment/packaging/docker/rpm-docker/build.sh
@@ -16,7 +16,17 @@
 #  limitations under the License.
 #
 
-rm -rf SRPMS/ RPMS/ && \
-rpmbuild -v -ba --define "_topdir $(pwd)" --define "_version 0.2.0" --define "_prerelease BETA" SPECS/metron.spec && \
-rpmlint -i SPECS/metron.spec RPMS/*/metron* SRPMS/metron
+# To avoid needing an epoch tag to update, BETA (or other prerelease tags) need to be in
+# Spec's release, not in the version.  Make sure this is split out based on the acutal version.
+# E.g. 0.2.0BETA becomes 0.2.0 version and BETA prerelease.
+# Empty string is acceptable when there is no prerelease tag.
+FULL_VERSION=$1
+echo "FULL_VERSION: ${FULL_VERSION}"
+VERSION=$(echo ${FULL_VERSION} | tr -d '"'"'[:alpha:]'"'"')
+echo "VERSION: ${VERSION}"
+PRERELEASE=$(echo ${FULL_VERSION} | tr -d '"'"'[:digit:]\.'"'"')
+echo "PRERELEASE: ${PRERELEASE}"
 
+rm -rf SRPMS/ RPMS/ && \
+rpmbuild -v -ba --define "_topdir $(pwd)" --define "_version ${VERSION}" --define "_prerelease ${PRERELEASE}" SPECS/metron.spec && \
+rpmlint -i SPECS/metron.spec RPMS/*/metron* SRPMS/metron
