6041acacedcc7b54bda51592ad5bf9161fa02e7e
Nick Allen <nick@nickallen.org>
2016-08-29 21:31:55 -0400

    METRON-389 Change to Context was lost in merge

--- metron-platform/metron-common/src/main/java/org/apache/metron/common/dsl/Context.java
+++ metron-platform/metron-common/src/main/java/org/apache/metron/common/dsl/Context.java
@@ -17,56 +17,62 @@
  */
 package org.apache.metron.common.dsl;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
 
-public class Context {
+public class Context implements Serializable {
+
   public interface Capability {
     Object get();
   }
-  public enum Capabilities {
-      HBASE_PROVIDER
-    , ZOOKEEPER_CLIENT
-    , SERVICE_DISCOVERER;
 
+  public enum Capabilities {
+    HBASE_PROVIDER,
+    ZOOKEEPER_CLIENT,
+    SERVICE_DISCOVERER
   }
 
   public static class Builder {
+
     private Map<String, Capability> capabilityMap = new HashMap<>();
 
     public Builder with(String s, Capability capability) {
       capabilityMap.put(s, capability);
       return this;
     }
+
     public Builder with(Enum<?> s, Capability capability) {
       capabilityMap.put(s.toString(), capability);
       return this;
     }
-    public Context build() {
 
+    public Context build() {
       return new Context(capabilityMap);
     }
   }
+
   public static Context EMPTY_CONTEXT() {
     return
-    new Context(new HashMap<>()){
-      @Override
-      public Optional<Object> getCapability(String capability) {
-        return Optional.empty();
-      }
-    };
+            new Context(new HashMap<>()){
+              @Override
+              public Optional<Object> getCapability(String capability) {
+                return Optional.empty();
+              }
+            };
   }
+
   private Map<String, Capability> capabilities;
-  private Context( Map<String, Capability> capabilities
-                 )
-  {
+
+  private Context( Map<String, Capability> capabilities) {
     this.capabilities = capabilities;
   }
+
   public Optional<Object> getCapability(Enum<?> capability) {
     return getCapability(capability.toString());
   }
+
   public Optional<Object> getCapability(String capability) {
     Capability c = capabilities.get(capability);
     if(c == null) {
