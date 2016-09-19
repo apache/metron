ffa7b5297f4013aa7aa364f49bae1c2eb6c4ee3a
cstella <cestella@gmail.com>
2016-09-19 11:13:07 -0400

    updating to have a handler for maps.

--- metron-platform/metron-parsers/src/test/java/org/apache/metron/parsers/json/JSONMapParserTest.java
+++ metron-platform/metron-parsers/src/test/java/org/apache/metron/parsers/json/JSONMapParserTest.java
@@ -1,5 +1,6 @@
 package org.apache.metron.parsers.json;
 
+import com.google.common.collect.ImmutableMap;
 import org.adrianwalker.multilinestring.Multiline;
 import org.json.simple.JSONObject;
 import org.junit.Assert;
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 public class JSONMapParserTest {
+
 
   /**
    {
@@ -38,14 +40,14 @@ public class JSONMapParserTest {
 
   /**
    {
-    "collection" : [ "blah" ]
+    "collection" : { "blah" : 7, "blah2" : "foo" }
    }
    */
    @Multiline
    static String collectionHandlingJSON;
 
   @Test
-  public void testCollectionHandling() {
+  public void testCollectionHandlingDrop() {
     JSONMapParser parser = new JSONMapParser();
     List<JSONObject> output = parser.parse(collectionHandlingJSON.getBytes());
     Assert.assertEquals(output.size(), 1);
@@ -56,5 +58,39 @@ public class JSONMapParserTest {
     Assert.assertTrue(message.get("timestamp") instanceof Number);
   }
 
+  @Test(expected=IllegalStateException.class)
+  public void testCollectionHandlingError() {
+    JSONMapParser parser = new JSONMapParser();
+    parser.configure(ImmutableMap.of("mapStrategy", "ERROR"));
+    List<JSONObject> output = parser.parse(collectionHandlingJSON.getBytes());
+  }
 
+
+  @Test
+  public void testCollectionHandlingAllow() {
+    JSONMapParser parser = new JSONMapParser();
+    parser.configure(ImmutableMap.of("mapStrategy", "ALLOW"));
+    List<JSONObject> output = parser.parse(collectionHandlingJSON.getBytes());
+    Assert.assertEquals(output.size(), 1);
+    //don't forget the timestamp field!
+    Assert.assertEquals(output.get(0).size(), 3);
+    JSONObject message = output.get(0);
+    Assert.assertNotNull(message.get("timestamp"));
+    Assert.assertTrue(message.get("timestamp") instanceof Number);
+  }
+
+  @Test
+  public void testCollectionHandlingUnfold() {
+    JSONMapParser parser = new JSONMapParser();
+    parser.configure(ImmutableMap.of("mapStrategy", "UNFOLD"));
+    List<JSONObject> output = parser.parse(collectionHandlingJSON.getBytes());
+    Assert.assertEquals(output.size(), 1);
+    //don't forget the timestamp field!
+    Assert.assertEquals(output.get(0).size(), 4);
+    JSONObject message = output.get(0);
+    Assert.assertEquals(message.get("collection.blah"), 7);
+    Assert.assertEquals(message.get("collection.blah2"), "foo");
+    Assert.assertNotNull(message.get("timestamp"));
+    Assert.assertTrue(message.get("timestamp") instanceof Number);
+  }
 }
