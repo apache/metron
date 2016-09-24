00a46745f316c21f8ea2c24caeda4d2c2239da97
cstella <cestella@gmail.com>
2016-09-24 11:40:36 -0400

    test typo.

--- metron-platform/metron-management/src/test/java/org/apache/metron/management/ConfigurationFunctionsTest.java
+++ metron-platform/metron-management/src/test/java/org/apache/metron/management/ConfigurationFunctionsTest.java
@@ -196,8 +196,16 @@ public class ConfigurationFunctionsTest {
   public void testEnrichmentPut() throws InterruptedException {
     String brop= (String) StellarTest.run("CONFIG_GET('ENRICHMENT', 'testEnrichmentPut')", new HashMap<>(), context);
     StellarTest.run("CONFIG_PUT('ENRICHMENT', config, 'testEnrichmentPut')", ImmutableMap.of("config", brop), context);
-    String bropNew= (String) StellarTest.run("CONFIG_GET('ENRICHMENT', 'testEnrichmentPut', false)", new HashMap<>(), context);
-    Assert.assertEquals(brop, bropNew);
+    boolean foundMatch = false;
+    for(int i = 0;i < 10 && !foundMatch;++i) {
+      String bropNew = (String) StellarTest.run("CONFIG_GET('ENRICHMENT', 'testEnrichmentPut', false)", new HashMap<>(), context);
+      foundMatch =  brop.equals(bropNew);
+      if(foundMatch) {
+        break;
+      }
+      Thread.sleep(2000);
+    }
+    Assert.assertTrue(foundMatch);
   }
 
   @Test(expected= ParseException.class)
@@ -210,11 +218,14 @@ public class ConfigurationFunctionsTest {
   @Test
   public void testParserPut() throws InterruptedException {
     String brop= (String) StellarTest.run("CONFIG_GET('PARSER', 'testParserPut')", new HashMap<>(), context);
-    StellarTest.run("CONFIG_PUT('PARSER', config, 'testEnrichmentPut')", ImmutableMap.of("config", brop), context);
+    StellarTest.run("CONFIG_PUT('PARSER', config, 'testParserPut')", ImmutableMap.of("config", brop), context);
     boolean foundMatch = false;
     for(int i = 0;i < 10 && !foundMatch;++i) {
-      String bropNew = (String) StellarTest.run("CONFIG_GET('PARSER', 'testEnrichmentPut', false)", new HashMap<>(), context);
+      String bropNew = (String) StellarTest.run("CONFIG_GET('PARSER', 'testParserPut', false)", new HashMap<>(), context);
       foundMatch =  brop.equals(bropNew);
+      if(foundMatch) {
+        break;
+      }
       Thread.sleep(2000);
     }
     Assert.assertTrue(foundMatch);
