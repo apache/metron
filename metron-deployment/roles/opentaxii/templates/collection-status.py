ee65589eea36be441cd19709524a4ba015b937a8
nsinha17 <nsinha@hortonworks.com>
2016-10-05 12:06:52 +0530

    METRON-484: Opentaxi service does not show count for subscribed services

--- metron-deployment/roles/opentaxii/templates/collection-status.py
+++ metron-deployment/roles/opentaxii/templates/collection-status.py
@@ -17,19 +17,17 @@
 #
 from cabby import create_client
 
+base_uri = 'http://localhost:9000'
 try:
-
     # create a connection
-    client = create_client('{{ opentaxii_domain }}', discovery_path='/services/discovery')
+    client = create_client(host='localhost',port=9000,discovery_path='/services/discovery')
 
     # iterate through each defined collection
-    collections = client.get_collections(uri='/services/collection')
+    collections = client.get_collections(uri='%s/services/collection'%base_uri)
+
     for collection in collections:
-
         # how many records in each collection?
-        count = client.get_content_count(collection_name=collection.name)
+        count = client.get_content_count(collection_name=collection.name, uri='%s/services/poll'%base_uri)
         print "%-50s %-10d" % (collection.name, count.count)
-
 except:
-
     print "Services not defined"
