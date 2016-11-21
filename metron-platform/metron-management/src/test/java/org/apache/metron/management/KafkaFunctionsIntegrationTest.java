4bc8b3e19e3a6df0118b9f6b93acb9b6d32f0c16
Otto Fowler <ofowler@industrialdefender.com>
2016-11-21 13:08:34 -0500

    add in the kafka fixes this time

--- metron-platform/metron-management/src/test/java/org/apache/metron/management/KafkaFunctionsIntegrationTest.java
+++ metron-platform/metron-management/src/test/java/org/apache/metron/management/KafkaFunctionsIntegrationTest.java
@@ -24,6 +24,7 @@ import org.apache.metron.common.stellar.StellarProcessor;
 import org.apache.metron.integration.BaseIntegrationTest;
 import org.apache.metron.integration.ComponentRunner;
 import org.apache.metron.integration.components.KafkaComponent;
+import org.apache.metron.integration.components.ZKServerComponent;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -51,6 +52,7 @@ public class KafkaFunctionsIntegrationTest extends BaseIntegrationTest {
   private static final String message3 = "{ \"ip_src_addr\": \"10.0.0.1\", \"value\": 29011 }";
 
   private static Map<String, Object> variables = new HashMap<>();
+  private static ZKServerComponent zkServerComponent;
   private static KafkaComponent kafkaComponent;
   private static ComponentRunner runner;
   private static Properties global;
@@ -59,12 +61,15 @@ public class KafkaFunctionsIntegrationTest extends BaseIntegrationTest {
   public static void setupKafka() throws Exception {
 
     Properties properties = new Properties();
+    zkServerComponent = getZKServerComponent(properties);
     kafkaComponent = getKafkaComponent(properties, new ArrayList<>());
 
     runner = new ComponentRunner.Builder()
+            .withComponent("zk", zkServerComponent)
             .withComponent("kafka", kafkaComponent)
             .withMillisecondsBetweenAttempts(5000)
             .withNumRetries(5)
+            .withCustomShutdownOrder(new String[]{"kafka","zk"})
             .build();
     runner.start();
   }
