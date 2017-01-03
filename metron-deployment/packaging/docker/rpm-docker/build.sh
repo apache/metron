72b0f7db4f9d7c4269e8a5f90100eed4ec3725be
kylerichardson <kylerichardson2@gmail.com>
2017-01-02 20:34:05 -0500

    METRON-644 Update rpm-docker build.sh
    Updated the script to account for non-existent user file ownership within
    the container.

--- metron-deployment/packaging/docker/rpm-docker/build.sh
+++ metron-deployment/packaging/docker/rpm-docker/build.sh
@@ -27,6 +27,16 @@ echo "VERSION: ${VERSION}"
 PRERELEASE=$(echo ${FULL_VERSION} | tr -d '"'"'[:digit:]\.'"'"')
 echo "PRERELEASE: ${PRERELEASE}"
 
+# Account for non-existent file owner in container
+OWNER_UID=`ls -n SPECS/metron.spec | awk -F' ' '{ print $3 }'`
+id $OWNER_UID >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  useradd -u $OWNER_UID builder
+fi
+
 rm -rf SRPMS/ RPMS/ && \
 rpmbuild -v -ba --define "_topdir $(pwd)" --define "_version ${VERSION}" --define "_prerelease ${PRERELEASE}" SPECS/metron.spec && \
 rpmlint -i SPECS/metron.spec RPMS/*/metron* SRPMS/metron
+
+# Ensure original user permissions are maintained after build
+chown -R $OWNER_UID *
