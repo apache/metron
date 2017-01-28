f8dd48ef920c948e1fc5ff736e386f641e551b2b
cstella <cestella@gmail.com>
2017-01-28 00:01:42 -0500

    Updating test to use a proper file

--- metron-platform/metron-common/src/test/java/org/apache/metron/common/utils/file/ReaderSpliteratorTest.java
+++ metron-platform/metron-common/src/test/java/org/apache/metron/common/utils/file/ReaderSpliteratorTest.java
@@ -19,10 +19,14 @@ package org.apache.metron.common.utils.file;
 
 import org.adrianwalker.multilinestring.Multiline;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.StringReader;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.StandardOpenOption;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
@@ -43,86 +47,108 @@ public class ReaderSpliteratorTest {
    */
   @Multiline
   public static String data;
+  public static final File dataFile = new File("target/readerspliteratortest.data");
 
-  @Test
-  public void testParallelStreamSmallBatch() {
-    Stream<String> stream = ReaderSpliterator.lineStream(new BufferedReader(new StringReader(data)), 2);
-    Map<String, Integer> count =
-            stream.parallel().map( s -> s.trim())
-                             .collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
-    Assert.assertEquals(5, count.size());
-    Assert.assertEquals(3, (int)count.get("foo"));
-    Assert.assertEquals(2, (int)count.get("bar"));
-    Assert.assertEquals(1, (int)count.get("and"));
-    Assert.assertEquals(1, (int)count.get("the"));
+  @BeforeClass
+  public static void setup() throws IOException {
+    if(dataFile.exists()) {
+      dataFile.delete();
+    }
+    Files.write(dataFile.toPath(), data.getBytes(), StandardOpenOption.CREATE_NEW, StandardOpenOption.TRUNCATE_EXISTING);
+    dataFile.deleteOnExit();
+  }
+
+  public static BufferedReader getReader() throws FileNotFoundException {
+    return new BufferedReader(new FileReader(dataFile));
   }
 
   @Test
-  public void testParallelStreamLargeBatch() {
-    Stream<String> stream = ReaderSpliterator.lineStream(new BufferedReader(new StringReader(data)), 100);
-    Map<String, Integer> count =
-            stream.parallel().map( s -> s.trim())
-                             .collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
-    Assert.assertEquals(5, count.size());
-    Assert.assertEquals(3, (int)count.get("foo"));
-    Assert.assertEquals(2, (int)count.get("bar"));
-    Assert.assertEquals(1, (int)count.get("and"));
-    Assert.assertEquals(1, (int)count.get("the"));
+  public void testParallelStreamSmallBatch() throws FileNotFoundException {
+    try( Stream<String> stream = ReaderSpliterator.lineStream(getReader(), 2)) {
+
+      Map<String, Integer> count =
+              stream.parallel().map( s -> s.trim())
+                      .collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
+      Assert.assertEquals(5, count.size());
+      Assert.assertEquals(3, (int)count.get("foo"));
+      Assert.assertEquals(2, (int)count.get("bar"));
+      Assert.assertEquals(1, (int)count.get("and"));
+      Assert.assertEquals(1, (int)count.get("the"));
+    }
   }
 
   @Test
-  public void testSequentialStreamLargeBatch() {
-    Stream<String> stream = ReaderSpliterator.lineStream(new BufferedReader(new StringReader(data)), 100);
-    Map<String, Integer> count =
-            stream.map( s -> s.trim())
-                  .collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
-    Assert.assertEquals(5, count.size());
-    Assert.assertEquals(3, (int)count.get("foo"));
-    Assert.assertEquals(2, (int)count.get("bar"));
-    Assert.assertEquals(1, (int)count.get("and"));
-    Assert.assertEquals(1, (int)count.get("the"));
+  public void testParallelStreamLargeBatch() throws FileNotFoundException {
+    try( Stream<String> stream = ReaderSpliterator.lineStream(getReader(), 100)) {
+      Map<String, Integer> count =
+              stream.parallel().map(s -> s.trim())
+                      .collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
+      Assert.assertEquals(5, count.size());
+      Assert.assertEquals(3, (int) count.get("foo"));
+      Assert.assertEquals(2, (int) count.get("bar"));
+      Assert.assertEquals(1, (int) count.get("and"));
+      Assert.assertEquals(1, (int) count.get("the"));
+    }
   }
 
   @Test
-  public void testActuallyParallel() throws ExecutionException, InterruptedException {
+  public void testSequentialStreamLargeBatch() throws FileNotFoundException {
+    try( Stream<String> stream = ReaderSpliterator.lineStream(getReader(), 100)) {
+      Map<String, Integer> count =
+              stream.map(s -> s.trim())
+                      .collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
+      Assert.assertEquals(5, count.size());
+      Assert.assertEquals(3, (int) count.get("foo"));
+      Assert.assertEquals(2, (int) count.get("bar"));
+      Assert.assertEquals(1, (int) count.get("and"));
+      Assert.assertEquals(1, (int) count.get("the"));
+    }
+  }
+
+  @Test
+  public void testActuallyParallel() throws ExecutionException, InterruptedException, FileNotFoundException {
     //With 9 elements and a batch of 2, we should only ceil(9/2) = 5 batches, so at most min(5, 2) = 2 threads will be used
-    Stream<String> stream = ReaderSpliterator.lineStream(new BufferedReader(new StringReader(data)), 2);
-    ForkJoinPool forkJoinPool = new ForkJoinPool(2);
-    forkJoinPool.submit(() -> {
-              Map<String, Integer> threads=
-                      stream.parallel().map(s -> Thread.currentThread().getName())
-                              .collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
-              Assert.assertTrue(threads.size() <= 2);
-            }
-    ).get();
+    try( Stream<String> stream = ReaderSpliterator.lineStream(getReader(), 2)) {
+      ForkJoinPool forkJoinPool = new ForkJoinPool(2);
+      forkJoinPool.submit(() -> {
+                Map<String, Integer> threads =
+                        stream.parallel().map(s -> Thread.currentThread().getName())
+                                .collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
+                Assert.assertTrue(threads.size() <= 2);
+              }
+      ).get();
+    }
   }
 
   @Test
-  public void testActuallyParallel_mediumBatch() throws ExecutionException, InterruptedException {
+  public void testActuallyParallel_mediumBatch() throws ExecutionException, InterruptedException, FileNotFoundException {
     //With 9 elements and a batch of 2, we should only ceil(9/2) = 5 batches, so at most 5 threads of the pool of 10 will be used
-    Stream<String> stream = ReaderSpliterator.lineStream(new BufferedReader(new StringReader(data)), 2);
-    ForkJoinPool forkJoinPool = new ForkJoinPool(10);
-    forkJoinPool.submit(() -> {
-              Map<String, Integer> threads=
-                      stream.parallel().map(s -> Thread.currentThread().getName())
-                              .collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
-              Assert.assertTrue(threads.size() <= (int)Math.ceil(9.0/2) && threads.size() > 1);
-            }
-    ).get();
+    try( Stream<String> stream = ReaderSpliterator.lineStream(getReader(), 2)) {
+      ForkJoinPool forkJoinPool = new ForkJoinPool(10);
+      forkJoinPool.submit(() -> {
+                Map<String, Integer> threads =
+                        stream.parallel().map(s -> Thread.currentThread().getName())
+                                .collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
+                Assert.assertTrue(threads.size() <= (int) Math.ceil(9.0 / 2) && threads.size() > 1);
+              }
+      ).get();
+    }
   }
 
   @Test
-  public void testActuallyParallel_bigBatch() throws ExecutionException, InterruptedException {
+  public void testActuallyParallel_bigBatch() throws ExecutionException, InterruptedException, FileNotFoundException {
     //With 9 elements and a batch of 10, we should only have one batch, so only one thread will be used
     //despite the thread pool size of 2.
-    Stream<String> stream = ReaderSpliterator.lineStream(new BufferedReader(new StringReader(data)), 10);
-    ForkJoinPool forkJoinPool = new ForkJoinPool(2);
-    forkJoinPool.submit(() -> {
-              Map<String, Integer> threads=
-                      stream.parallel().map(s -> Thread.currentThread().getName())
-                              .collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
-              Assert.assertEquals(1, threads.size());
-            }
-    ).get();
+    try( Stream<String> stream = ReaderSpliterator.lineStream(getReader(), 10)) {
+      ForkJoinPool forkJoinPool = new ForkJoinPool(2);
+      forkJoinPool.submit(() -> {
+                Map<String, Integer> threads =
+                        stream.parallel().map(s -> Thread.currentThread().getName())
+                                .collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
+                Assert.assertEquals(1, threads.size());
+              }
+      ).get();
+    }
   }
+
 }
