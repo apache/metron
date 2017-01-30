8ea6ae57739226d80a97bf849f80cc3c2a7ebac4
JJ <jjmeyer0@gmail.com>
2017-01-30 06:27:43 -0600

    METRON-503: Updated DockerStormCLIWrapper to mock all process builders.

--- metron-interface/metron-rest/src/test/java/org/apache/metron/rest/service/impl/DockerStormCLIWrapperTest.java
+++ metron-interface/metron-rest/src/test/java/org/apache/metron/rest/service/impl/DockerStormCLIWrapperTest.java
@@ -2,18 +2,34 @@ package org.apache.metron.rest.service.impl;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.core.env.Environment;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyVararg;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
+
+@SuppressWarnings("unchecked")
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({DockerStormCLIWrapper.class, ProcessBuilder.class})
 public class DockerStormCLIWrapperTest {
+  private ProcessBuilder processBuilder;
   private Environment environment;
   private DockerStormCLIWrapper dockerStormCLIWrapper;
 
   @Before
   public void setUp() throws Exception {
+    processBuilder = mock(ProcessBuilder.class);
     environment = mock(Environment.class);
 
     dockerStormCLIWrapper = new DockerStormCLIWrapper(environment);
@@ -21,17 +37,30 @@ public class DockerStormCLIWrapperTest {
 
   @Test
   public void getProcessBuilderShouldProperlyGenerateProcessorBuilder() throws Exception {
-    ProcessBuilder expectedProcessBuilder = new ProcessBuilder("docker-compose", "-f", "/test", "-p", "metron", "exec", "storm", "oo", "ooo");
-    expectedProcessBuilder.environment().put("METRON_VERSION", "1");
+    whenNew(ProcessBuilder.class).withParameterTypes(String[].class).withArguments(anyVararg()).thenReturn(processBuilder);
 
+    when(processBuilder.environment()).thenReturn(new HashMap<>());
+    when(processBuilder.command()).thenReturn(new ArrayList<>());
 
+    Process process = mock(Process.class);
+    InputStream inputStream = new InputStream() {
+      @Override
+      public int read() throws IOException {
+        return -1;
+      }
+    };
+
+    when(processBuilder.start()).thenReturn(process);
+    when(process.getInputStream()).thenReturn(inputStream);
     when(environment.getProperty("docker.compose.path")).thenReturn("/test");
     when(environment.getProperty("metron.version")).thenReturn("1");
 
 
     ProcessBuilder actualBuilder = dockerStormCLIWrapper.getProcessBuilder("oo", "ooo");
 
-    assertEquals(expectedProcessBuilder.environment(), actualBuilder.environment());
-    assertEquals(expectedProcessBuilder.command(), actualBuilder.command());
+    assertEquals(new HashMap<String, String>() {{ put("METRON_VERSION", "1"); }}, actualBuilder.environment());
+    assertEquals(new ArrayList<>(), actualBuilder.command());
+
+    verify(process).waitFor();
   }
 }
