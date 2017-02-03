ad1aef760948109565b7144479151312ebccc24d
Michael Miklavcic <michael.miklavcic@gmail.com>
2017-02-03 12:46:05 -0700

    Get integration tests working for Stellar transformations in the file loader

--- metron-platform/metron-data-management/src/test/java/org/apache/metron/dataloads/nonbulk/flatfile/SimpleEnrichmentFlatFileLoaderIntegrationTest.java
+++ metron-platform/metron-data-management/src/test/java/org/apache/metron/dataloads/nonbulk/flatfile/SimpleEnrichmentFlatFileLoaderIntegrationTest.java
@@ -20,6 +20,7 @@ package org.apache.metron.dataloads.nonbulk.flatfile;
 import org.adrianwalker.multilinestring.Multiline;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.PosixParser;
+import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.test.TestingServer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -31,6 +32,7 @@ import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.metron.common.configuration.ConfigurationsUtils;
 import org.apache.metron.dataloads.hbase.mr.HBaseUtil;
 import org.apache.metron.enrichment.converter.EnrichmentConverter;
 import org.apache.metron.enrichment.converter.EnrichmentKey;
@@ -65,8 +67,9 @@ public class SimpleEnrichmentFlatFileLoaderIntegrationTest {
   /** The test table. */
   private static HTable testTable;
   private static Configuration config = null;
-  private TestingServer testZkServer;
-  private String zookeeperUrl;
+  private static TestingServer testZkServer;
+  private static String zookeeperUrl;
+  private static CuratorFramework client;
   private static final String tableName = "enrichment";
   private static final String cf = "cf";
   private static final String csvFile="input.csv";
@@ -82,6 +85,14 @@ public class SimpleEnrichmentFlatFileLoaderIntegrationTest {
   private static final File wholeFileExtractorConfigFile = new File("target/sefflt_extractorConfig_wf.json");
   private static final File stellarExtractorConfigFile = new File("target/sefflt_extractorConfig_stellar.json");
   private static final int NUM_LINES = 1000;
+
+  /**
+   * {
+   *   "enrichment_property" : "valfromglobalconfig"
+   * }
+   */
+  @Multiline
+  public static String globalConfig;
 
   /**
    {
@@ -121,12 +132,15 @@ public class SimpleEnrichmentFlatFileLoaderIntegrationTest {
   /**
    *{
    *  "config" : {
+   *    "zk_quorum" : "%ZK_QUORUM%",
    *    "columns" : {
    *      "host" : 0,
+   *      "empty" : 1,
    *      "meta" : 2
    *    },
    *    "value_transform" : {
-   *      "host" : "TO_UPPER(host)"
+   *      "host" : "TO_UPPER(host)",
+   *      "empty" : "enrichment_property"
    *    },
    *    "value_filter" : "LENGTH(host) > 0",
    *    "indicator_column" : "host",
@@ -150,6 +164,8 @@ public class SimpleEnrichmentFlatFileLoaderIntegrationTest {
     config = kv.getValue();
     testUtil = kv.getKey();
     testTable = testUtil.createTable(Bytes.toBytes(tableName), Bytes.toBytes(cf));
+    zookeeperUrl = getZookeeperUrl(config.get("hbase.zookeeper.quorum"), testUtil.getZkCluster().getClientPort());
+    setupGlobalConfig(zookeeperUrl);
 
     for(Result r : testTable.getScanner(Bytes.toBytes(cf))) {
       Delete d = new Delete(r.getRow());
@@ -174,7 +190,7 @@ public class SimpleEnrichmentFlatFileLoaderIntegrationTest {
       stellarExtractorConfigFile.delete();
     }
     Files.write( stellarExtractorConfigFile.toPath()
-               , stellarExtractorConfig.getBytes()
+               , stellarExtractorConfig.replace("%ZK_QUORUM%", zookeeperUrl).getBytes()
                , StandardOpenOption.CREATE_NEW , StandardOpenOption.TRUNCATE_EXISTING
     );
     if(file1.exists()) {
@@ -223,6 +239,16 @@ public class SimpleEnrichmentFlatFileLoaderIntegrationTest {
       }
     }
 
+  }
+
+  private static String getZookeeperUrl(String host, int port) {
+    return host + ":" + port;
+  }
+
+  private static void setupGlobalConfig(String zookeeperUrl) throws Exception {
+    client = ConfigurationsUtils.getClient(zookeeperUrl);
+    client.start();
+    ConfigurationsUtils.writeGlobalConfigToZookeeper(globalConfig.getBytes(), zookeeperUrl);
   }
 
   @AfterClass
@@ -382,7 +408,7 @@ public class SimpleEnrichmentFlatFileLoaderIntegrationTest {
   }
 
   @Test
-  public void transforms_fields() throws Exception {
+  public void stellar_transforms_and_filters_indicators_and_value_metadata() throws Exception {
     String[] argv = {"-c cf", "-t enrichment"
             , "-e " + stellarExtractorConfigFile.getPath()
             , "-i " + multilineFile.getPath()
@@ -399,8 +425,9 @@ public class SimpleEnrichmentFlatFileLoaderIntegrationTest {
     Assert.assertEquals(NUM_LINES, results.size());
     Assert.assertThat(results.get(0).getKey().getIndicator(), startsWith("GOOGLE"));
     Assert.assertThat(results.get(0).getKey().type, equalTo("enrichment"));
-    Assert.assertThat(results.get(0).getValue().getMetadata().size(), equalTo(2));
+    Assert.assertThat(results.get(0).getValue().getMetadata().size(), equalTo(3));
     Assert.assertThat(results.get(0).getValue().getMetadata().get("meta").toString(), startsWith("foo"));
+    Assert.assertThat(results.get(0).getValue().getMetadata().get("empty").toString(), startsWith("valfromglobalconfig"));
     Assert.assertThat(results.get(0).getValue().getMetadata().get("host").toString(), startsWith("GOOGLE"));
   }
 
