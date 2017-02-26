57928349c9a5e0af9653c694672decc590b4e590
cstella <cestella@gmail.com>
2017-02-25 23:32:02 -0500

    METRON-742: Generated code for profile window selector DSL did not get committed as part of METRON-690

--- metron-analytics/metron-profiler-client/src/main/java/org/apache/metron/profiler/client/window/generated/WindowListener.java
+++ metron-analytics/metron-profiler-client/src/main/java/org/apache/metron/profiler/client/window/generated/WindowListener.java
@@ -24,208 +24,208 @@ import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 
 /**
- * This interface defines a complete listener for a process tree produced by
+ * This interface defines a complete listener for a parse tree produced by
  * {@link WindowParser}.
  */
 public interface WindowListener extends ParseTreeListener {
 	/**
-	 * Enter a process tree produced by {@link WindowParser#window}.
-	 * @param ctx the process tree
+	 * Enter a parse tree produced by {@link WindowParser#window}.
+	 * @param ctx the parse tree
 	 */
 	void enterWindow(WindowParser.WindowContext ctx);
 	/**
-	 * Exit a process tree produced by {@link WindowParser#window}.
-	 * @param ctx the process tree
+	 * Exit a parse tree produced by {@link WindowParser#window}.
+	 * @param ctx the parse tree
 	 */
 	void exitWindow(WindowParser.WindowContext ctx);
 	/**
-	 * Enter a process tree produced by the {@code NonRepeatingWindow}
+	 * Enter a parse tree produced by the {@code NonRepeatingWindow}
 	 * labeled alternative in {@link WindowParser#window_expression}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void enterNonRepeatingWindow(WindowParser.NonRepeatingWindowContext ctx);
 	/**
-	 * Exit a process tree produced by the {@code NonRepeatingWindow}
+	 * Exit a parse tree produced by the {@code NonRepeatingWindow}
 	 * labeled alternative in {@link WindowParser#window_expression}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void exitNonRepeatingWindow(WindowParser.NonRepeatingWindowContext ctx);
 	/**
-	 * Enter a process tree produced by the {@code RepeatingWindow}
+	 * Enter a parse tree produced by the {@code RepeatingWindow}
 	 * labeled alternative in {@link WindowParser#window_expression}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void enterRepeatingWindow(WindowParser.RepeatingWindowContext ctx);
 	/**
-	 * Exit a process tree produced by the {@code RepeatingWindow}
+	 * Exit a parse tree produced by the {@code RepeatingWindow}
 	 * labeled alternative in {@link WindowParser#window_expression}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void exitRepeatingWindow(WindowParser.RepeatingWindowContext ctx);
 	/**
-	 * Enter a process tree produced by the {@code DenseWindow}
+	 * Enter a parse tree produced by the {@code DenseWindow}
 	 * labeled alternative in {@link WindowParser#window_expression}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void enterDenseWindow(WindowParser.DenseWindowContext ctx);
 	/**
-	 * Exit a process tree produced by the {@code DenseWindow}
+	 * Exit a parse tree produced by the {@code DenseWindow}
 	 * labeled alternative in {@link WindowParser#window_expression}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void exitDenseWindow(WindowParser.DenseWindowContext ctx);
 	/**
-	 * Enter a process tree produced by {@link WindowParser#excluding_specifier}.
-	 * @param ctx the process tree
+	 * Enter a parse tree produced by {@link WindowParser#excluding_specifier}.
+	 * @param ctx the parse tree
 	 */
 	void enterExcluding_specifier(WindowParser.Excluding_specifierContext ctx);
 	/**
-	 * Exit a process tree produced by {@link WindowParser#excluding_specifier}.
-	 * @param ctx the process tree
+	 * Exit a parse tree produced by {@link WindowParser#excluding_specifier}.
+	 * @param ctx the parse tree
 	 */
 	void exitExcluding_specifier(WindowParser.Excluding_specifierContext ctx);
 	/**
-	 * Enter a process tree produced by {@link WindowParser#including_specifier}.
-	 * @param ctx the process tree
+	 * Enter a parse tree produced by {@link WindowParser#including_specifier}.
+	 * @param ctx the parse tree
 	 */
 	void enterIncluding_specifier(WindowParser.Including_specifierContext ctx);
 	/**
-	 * Exit a process tree produced by {@link WindowParser#including_specifier}.
-	 * @param ctx the process tree
+	 * Exit a parse tree produced by {@link WindowParser#including_specifier}.
+	 * @param ctx the parse tree
 	 */
 	void exitIncluding_specifier(WindowParser.Including_specifierContext ctx);
 	/**
-	 * Enter a process tree produced by {@link WindowParser#specifier}.
-	 * @param ctx the process tree
+	 * Enter a parse tree produced by {@link WindowParser#specifier}.
+	 * @param ctx the parse tree
 	 */
 	void enterSpecifier(WindowParser.SpecifierContext ctx);
 	/**
-	 * Exit a process tree produced by {@link WindowParser#specifier}.
-	 * @param ctx the process tree
+	 * Exit a parse tree produced by {@link WindowParser#specifier}.
+	 * @param ctx the parse tree
 	 */
 	void exitSpecifier(WindowParser.SpecifierContext ctx);
 	/**
-	 * Enter a process tree produced by {@link WindowParser#specifier_arg_list}.
-	 * @param ctx the process tree
+	 * Enter a parse tree produced by {@link WindowParser#specifier_arg_list}.
+	 * @param ctx the parse tree
 	 */
 	void enterSpecifier_arg_list(WindowParser.Specifier_arg_listContext ctx);
 	/**
-	 * Exit a process tree produced by {@link WindowParser#specifier_arg_list}.
-	 * @param ctx the process tree
+	 * Exit a parse tree produced by {@link WindowParser#specifier_arg_list}.
+	 * @param ctx the parse tree
 	 */
 	void exitSpecifier_arg_list(WindowParser.Specifier_arg_listContext ctx);
 	/**
-	 * Enter a process tree produced by {@link WindowParser#day_specifier}.
-	 * @param ctx the process tree
+	 * Enter a parse tree produced by {@link WindowParser#day_specifier}.
+	 * @param ctx the parse tree
 	 */
 	void enterDay_specifier(WindowParser.Day_specifierContext ctx);
 	/**
-	 * Exit a process tree produced by {@link WindowParser#day_specifier}.
-	 * @param ctx the process tree
+	 * Exit a parse tree produced by {@link WindowParser#day_specifier}.
+	 * @param ctx the parse tree
 	 */
 	void exitDay_specifier(WindowParser.Day_specifierContext ctx);
 	/**
-	 * Enter a process tree produced by {@link WindowParser#identifier}.
-	 * @param ctx the process tree
+	 * Enter a parse tree produced by {@link WindowParser#identifier}.
+	 * @param ctx the parse tree
 	 */
 	void enterIdentifier(WindowParser.IdentifierContext ctx);
 	/**
-	 * Exit a process tree produced by {@link WindowParser#identifier}.
-	 * @param ctx the process tree
+	 * Exit a parse tree produced by {@link WindowParser#identifier}.
+	 * @param ctx the parse tree
 	 */
 	void exitIdentifier(WindowParser.IdentifierContext ctx);
 	/**
-	 * Enter a process tree produced by {@link WindowParser#specifier_list}.
-	 * @param ctx the process tree
+	 * Enter a parse tree produced by {@link WindowParser#specifier_list}.
+	 * @param ctx the parse tree
 	 */
 	void enterSpecifier_list(WindowParser.Specifier_listContext ctx);
 	/**
-	 * Exit a process tree produced by {@link WindowParser#specifier_list}.
-	 * @param ctx the process tree
+	 * Exit a parse tree produced by {@link WindowParser#specifier_list}.
+	 * @param ctx the parse tree
 	 */
 	void exitSpecifier_list(WindowParser.Specifier_listContext ctx);
 	/**
-	 * Enter a process tree produced by the {@code FromToDuration}
+	 * Enter a parse tree produced by the {@code FromToDuration}
 	 * labeled alternative in {@link WindowParser#duration}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void enterFromToDuration(WindowParser.FromToDurationContext ctx);
 	/**
-	 * Exit a process tree produced by the {@code FromToDuration}
+	 * Exit a parse tree produced by the {@code FromToDuration}
 	 * labeled alternative in {@link WindowParser#duration}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void exitFromToDuration(WindowParser.FromToDurationContext ctx);
 	/**
-	 * Enter a process tree produced by the {@code FromDuration}
+	 * Enter a parse tree produced by the {@code FromDuration}
 	 * labeled alternative in {@link WindowParser#duration}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void enterFromDuration(WindowParser.FromDurationContext ctx);
 	/**
-	 * Exit a process tree produced by the {@code FromDuration}
+	 * Exit a parse tree produced by the {@code FromDuration}
 	 * labeled alternative in {@link WindowParser#duration}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void exitFromDuration(WindowParser.FromDurationContext ctx);
 	/**
-	 * Enter a process tree produced by the {@code SkipDistance}
+	 * Enter a parse tree produced by the {@code SkipDistance}
 	 * labeled alternative in {@link WindowParser#skip_distance}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void enterSkipDistance(WindowParser.SkipDistanceContext ctx);
 	/**
-	 * Exit a process tree produced by the {@code SkipDistance}
+	 * Exit a parse tree produced by the {@code SkipDistance}
 	 * labeled alternative in {@link WindowParser#skip_distance}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void exitSkipDistance(WindowParser.SkipDistanceContext ctx);
 	/**
-	 * Enter a process tree produced by the {@code WindowWidth}
+	 * Enter a parse tree produced by the {@code WindowWidth}
 	 * labeled alternative in {@link WindowParser#window_width}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void enterWindowWidth(WindowParser.WindowWidthContext ctx);
 	/**
-	 * Exit a process tree produced by the {@code WindowWidth}
+	 * Exit a parse tree produced by the {@code WindowWidth}
 	 * labeled alternative in {@link WindowParser#window_width}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void exitWindowWidth(WindowParser.WindowWidthContext ctx);
 	/**
-	 * Enter a process tree produced by the {@code TimeInterval}
+	 * Enter a parse tree produced by the {@code TimeInterval}
 	 * labeled alternative in {@link WindowParser#time_interval}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void enterTimeInterval(WindowParser.TimeIntervalContext ctx);
 	/**
-	 * Exit a process tree produced by the {@code TimeInterval}
+	 * Exit a parse tree produced by the {@code TimeInterval}
 	 * labeled alternative in {@link WindowParser#time_interval}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void exitTimeInterval(WindowParser.TimeIntervalContext ctx);
 	/**
-	 * Enter a process tree produced by the {@code TimeAmount}
+	 * Enter a parse tree produced by the {@code TimeAmount}
 	 * labeled alternative in {@link WindowParser#time_amount}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void enterTimeAmount(WindowParser.TimeAmountContext ctx);
 	/**
-	 * Exit a process tree produced by the {@code TimeAmount}
+	 * Exit a parse tree produced by the {@code TimeAmount}
 	 * labeled alternative in {@link WindowParser#time_amount}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void exitTimeAmount(WindowParser.TimeAmountContext ctx);
 	/**
-	 * Enter a process tree produced by the {@code TimeUnit}
+	 * Enter a parse tree produced by the {@code TimeUnit}
 	 * labeled alternative in {@link WindowParser#time_unit}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void enterTimeUnit(WindowParser.TimeUnitContext ctx);
 	/**
-	 * Exit a process tree produced by the {@code TimeUnit}
+	 * Exit a parse tree produced by the {@code TimeUnit}
 	 * labeled alternative in {@link WindowParser#time_unit}.
-	 * @param ctx the process tree
+	 * @param ctx the parse tree
 	 */
 	void exitTimeUnit(WindowParser.TimeUnitContext ctx);
 }
