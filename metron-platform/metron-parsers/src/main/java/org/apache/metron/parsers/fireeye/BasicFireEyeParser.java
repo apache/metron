68bbb5204fe4248d07ebc5eaeb839bc21d889467
Vladimir Sh <bjigmp@users.noreply.github.com>
2017-04-10 20:42:36 +0300

    Set correct TS for FireEye parser

--- metron-platform/metron-parsers/src/main/java/org/apache/metron/parsers/fireeye/BasicFireEyeParser.java
+++ metron-platform/metron-parsers/src/main/java/org/apache/metron/parsers/fireeye/BasicFireEyeParser.java
@@ -122,14 +122,12 @@ public class BasicFireEyeParser extends BasicParser {
 			month = tsMatcher.group(1);
 			day = tsMatcher.group(2);
 			time = tsMatcher.group(3);
-	
-				} else {
-			LOG.warn("Unable to find timestamp in message: " + toParse);
 			ts = ParserUtils.convertToEpoch(month, day, time, true);
+		} else {
+			LOG.warn("Unable to find timestamp in message: " + toParse);
 		}
 
-			return ts;
-	
+		return ts;
 	}
 
 	private JSONObject parseMessage(String toParse) {
