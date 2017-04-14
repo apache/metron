6e23510bd44638b8f446c7ccdb78f7aefa254d0a
Bas van de Lustgraaf <bas@lustgraaf.nl>
2017-04-14 20:52:18 +0200

    METRON-854 Create DHCPDump Parser Fixed test class by replacing @BeforeClass

--- metron-platform/metron-parsers/src/test/java/org/apache/metron/parsers/dhcpdump/BasicDHCPDumpParserTest.java
+++ metron-platform/metron-parsers/src/test/java/org/apache/metron/parsers/dhcpdump/BasicDHCPDumpParserTest.java
@@ -22,20 +22,23 @@ import junit.framework.TestCase;
 import org.apache.metron.parsers.ParseException;
 import org.json.simple.JSONObject;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
 
-public class BasicDHCPDumpParserTest  extends TestCase {
+public class BasicDHCPDumpParserTest extends TestCase {
 
     /**
      * The parser.
      */
-    private static BasicDHCPDumpParser DHCPDumpParser = null;
+    private BasicDHCPDumpParser DHCPDumpParser = null;
 
-    @BeforeClass
-    public static void setUpOnce() throws Exception {
+    /**
+     * Constructs a new <code>BasicDHCPDumpParserTest</code> instance.
+     *
+     * @throws Exception
+     */
+    public BasicDHCPDumpParserTest() throws Exception {
         DHCPDumpParser = new BasicDHCPDumpParser();
     }
 
@@ -43,15 +46,15 @@ public class BasicDHCPDumpParserTest  extends TestCase {
     public void testSimpleMessage() throws ParseException, IOException {
         String rawMessage = "TIME: 2017-01-16 17:39:36.581|INTERFACE: eth2|OP:1 BOOTPREQUEST|CIADDR: 172.20.75.77|YIADDR: 0.0.0.0|SIADDR: 0.0.0.0|GIADDR: 172.20.75.8|CHADDR: fc:f8:ae:e8:ef:db:00:00:00:00:00:00:00:00:00:00|OPTION:  53   1 DHCP message type: 8 |DHCPINFORM|OPTION:  61   7 Client-identifier: 01:fc:f8:ae:e8:ef:db|OPTION:  12   5 Host name: A1244|OPTION:  60   8 Vendor class identifier: MSFT 5.0|OPTION:  55  13 Parameter Request List:   1 (Subnet mask)|| 15 (Domainname)||  3 (Routers)||  6 (DNS server)|| 44 (NetBIOS name server)|| 46 (NetBIOS node type)|| 47 (NetBIOS scope)|| 31 (Perform router discovery)|| 33 (Static route)||121 (Classless Static Route)||249 (MSFT - Classless route)|| 43 (Vendor specific info)||252 (MSFT - WinSock Proxy Auto Detect)|||IP: 100.100.100.177 > 172.20.1.11 | b8:ca:3a:67:95:8a > 0:50:56:84:68:43";
 
-        JSONObject keyValueJson = DHCPDumpParser.parse(rawMessage.getBytes()).get(0);
+        JSONObject metronJSONObject = DHCPDumpParser.parse(rawMessage.getBytes()).get(0);
 
-        Assert.assertEquals(keyValueJson.get("original_string").toString(), rawMessage);
-        Assert.assertEquals(keyValueJson.get("timestamp").toString(), "1484584776581");
+        Assert.assertEquals(metronJSONObject.get("original_string").toString(), rawMessage);
+        Assert.assertEquals(metronJSONObject.get("timestamp").toString(), "1484584776581");
 
-        Assert.assertEquals(keyValueJson.get("ip_src_addr").toString(), "100.100.100.177");
-        Assert.assertEquals(keyValueJson.get("ip_dst_addr").toString(), "172.20.1.11");
+        Assert.assertEquals(metronJSONObject.get("ip_src_addr").toString(), "100.100.100.177");
+        Assert.assertEquals(metronJSONObject.get("ip_dst_addr").toString(), "172.20.1.11");
 
-        Assert.assertEquals(keyValueJson.get("time").toString(), "2017-01-16 17:39:36.581");
+        Assert.assertEquals(metronJSONObject.get("time").toString(), "2017-01-16 17:39:36.581");
     }
 
 
