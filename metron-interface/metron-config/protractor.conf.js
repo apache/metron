be6ec528c9d0ac37014bb28220f31af7c7b4ef0f
iraghumitra <raghumitra.ksv@gmail.com>
2017-04-20 15:17:11 +0530

    Removed usage of setSize b'coz of https://github.com/SeleniumHQ/selenium/issues/3508

--- metron-interface/metron-config/protractor.conf.js
+++ metron-interface/metron-config/protractor.conf.js
@@ -62,15 +62,5 @@ exports.config = {
     jasmine.getEnv().addReporter(new SpecReporter({
       displayStacktrace: 'specs'
     }));
-    setTimeout(function() {
-      browser.driver.executeScript(function() {
-        return {
-          width: window.screen.availWidth,
-          height: window.screen.availHeight
-        };
-      }).then(function(result) {
-        browser.driver.manage().window().setSize(result.width, result.height);
-      });
-    });
   }
 };
