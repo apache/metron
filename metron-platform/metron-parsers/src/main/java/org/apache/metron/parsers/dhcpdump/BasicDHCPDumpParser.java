af4f60196ca7f37075b03a3f43776990ca3f0f4b
Bas van de Lustgraaf <bas@lustgraaf.nl>
2017-04-25 09:51:43 +0200

    METRON-854 Changed the way how to split the log messages, small format / structure changes

--- metron-platform/metron-parsers/src/main/java/org/apache/metron/parsers/dhcpdump/BasicDHCPDumpParser.java
+++ metron-platform/metron-parsers/src/main/java/org/apache/metron/parsers/dhcpdump/BasicDHCPDumpParser.java
@@ -15,10 +15,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.metron.parsers.dhcpdump;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.metron.common.Constants;
 import org.apache.metron.parsers.BasicParser;
 import org.json.simple.JSONObject;
@@ -32,6 +30,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+/**
+ * NOTE: This parser is not compatible with the original DHCPDump-1.8 output.
+ * Please modify the DHCPDump output to match the required format or download the source
+ * from github (https://github.com/basvdl/dhcpdump/tree/master).
+ *
+ * Format changes:
+ *      - new lines `\n` were replaced by pipes `|`
+ *      - the parameters list (printReqParmList()) is separated by commas `,` instead of new lines `\n`
+ *
+ * @input TIME: 2017-04-21 15:43:17.673|IP: 172.20.3.28 (84:2b:2b:86:62:43) > 255.255.255.255 (ff:ff:ff:ff:ff:ff)|OP: 1 (BOOTPREQUEST)|HTYPE: 1 (Ethernet)|HLEN: 6|HOPS: 0|XID: 23850fd5|SECS: 0|FLAGS: 0|CIADDR: 172.20.3.28|YIADDR: 0.0.0.0|SIADDR: 0.0.0.0|GIADDR: 0.0.0.0|CHADDR: 84:2b:2b:86:62:43:00:00:00:00:00:00:00:00:00:00|SNAME: .|FNAME: .|OPTION:  53 (  1) DHCP message type         8 (DHCPINFORM)|OPTION:  61 (  7) Client-identifier         01:84:2b:2b:86:62:43|OPTION:  12 (  5) Host name                 Q0619|OPTION:  60 (  8) Vendor class identifier   MSFT 5.0|OPTION:  55 ( 13) Parameter Request List    1 (Subnet mask),15 (Domainname),3 (Routers),6 (DNS server),44 (NetBIOS name server),46 (NetBIOS node type),47 (NetBIOS scope),31 (Perform router discovery),33 (Static route),121 (Classless Static Route),249 (MSFT - Classless route),43 (Vendor specific info),252 (MSFT - WinSock Proxy Auto Detect)
+ */
 public class BasicDHCPDumpParser  extends BasicParser {
 
   protected static final Logger LOG = LoggerFactory.getLogger(BasicDHCPDumpParser.class);
@@ -96,15 +105,8 @@ public class BasicDHCPDumpParser  extends BasicParser {
     return messages;
   }
 
-
   private String[] convertToMultiLine(String msg) {
-
-    msg = msg.replace("||", "");
-
-    int count = StringUtils.countMatches(msg, "|");
-
-    /* TODO find better method to prevent splitting the IP: field content,currently this works only when IP: is the last field in the event */
-    String[] multiLineEvent = msg.split(Pattern.quote("|"), count);
+    String[] multiLineEvent = msg.split(Pattern.quote("|"));
 
     return multiLineEvent;
   }
@@ -115,18 +117,6 @@ public class BasicDHCPDumpParser  extends BasicParser {
     for (String s: multiLineEvent) {
       if (s.startsWith("TIME:")) {
         keyValueEvent.put("time", s.split(":", 2)[1].trim());
-      } else if (s.startsWith("IP:")) {
-        String value = s.split(":", 2)[1];
-        value = value.replace("|", ">");
-
-        String[] values = value.split(">");
-
-        keyValueEvent.put(Constants.Fields.SRC_ADDR.getName(), values[0].trim());
-        keyValueEvent.put(Constants.Fields.DST_ADDR.getName(), values[1].trim());
-        keyValueEvent.put("mac_src_addr", values[2].trim());
-        keyValueEvent.put("mac_dst_addr", values[3].trim());
-      } else if (s.startsWith("OP:")) {
-        keyValueEvent.put("op", s.split(":")[1].split(" ")[1].trim());
       } else if (s.startsWith("CIADDR:")) {
         keyValueEvent.put("ciaddr", s.split(":", 2)[1].trim());
       } else if (s.startsWith("YIADDR:")) {
@@ -135,21 +125,44 @@ public class BasicDHCPDumpParser  extends BasicParser {
         keyValueEvent.put("siaddr", s.split(":", 2)[1].trim());
       } else if (s.startsWith("GIADDR:")) {
         keyValueEvent.put("giaddr", s.split(":", 2)[1].trim());
+      } else if (s.startsWith("INTERFACE:")) {
+        keyValueEvent.put("interface", s.split(":", 2)[1].trim());
+      } else if (s.startsWith("IP:")) {
+        /**
+         * Input example
+         * IP: 172.20.3.28 (84:2b:2b:86:62:43) > 255.255.255.255 (ff:ff:ff:ff:ff:ff)
+         */
+        String value = s.split(":", 2)[1].trim();
+        value = value.replaceAll("\\(|\\)|>\\s", ""); // replace `(`, `)` and `> `
+
+        String[] values = value.split(" ");
+
+        keyValueEvent.put(Constants.Fields.SRC_ADDR.getName(), values[0].trim());
+        keyValueEvent.put(Constants.Fields.DST_ADDR.getName(), values[2].trim());
+        keyValueEvent.put("mac_src_addr", values[1].trim());
+        keyValueEvent.put("mac_dst_addr", values[3].trim());
+      } else if (s.startsWith("OP:")) {
+        /**
+         * Input example
+         * OP: 1 (BOOTPREQUEST)
+         */
+        keyValueEvent.put("op", s.split(":")[1].split(" ")[1].trim());
       } else if (s.startsWith("OPTION:")) {
         /**
          * Input examples
-         * OPTION:  12   5 Host name: A1244
-         * OPTION:  61   7 Client-identifier: 01:fc:f8:ae:e8:ef:db
+         * OPTION:  12 (  5) Host name                 Q0619
+         * OPTION:  61 (  7) Client-identifier         01:84:2b:2b:86:62:43
          */
-        Integer op = Integer.valueOf(s.split(":")[1].trim().split(" ")[0]);
+        Integer op = Integer.valueOf(s.substring(7, 12).trim());
 
         Integer[] validOptions = {12, 61};
 
         if (Arrays.asList(validOptions).contains(op)) {
+          String value = s.substring(44, s.length()).trim();
           if (op == 12) {
-            keyValueEvent.put("host_name", s.split(":", 3)[2].trim());
+            keyValueEvent.put("host_name", value);
           } else if (op == 61) {
-            keyValueEvent.put("client_identifier", s.split(":", 3)[2].trim());
+            keyValueEvent.put("client_identifier", value);
           }
         }
 
@@ -158,7 +171,6 @@ public class BasicDHCPDumpParser  extends BasicParser {
 
     return keyValueEvent;
   }
-
 
   private long timestampToEpoch(String timestamp) throws java.text.ParseException {
     return timestampFormat.parse(timestamp).getTime();
--- metron-platform/metron-parsers/src/test/java/org/apache/metron/parsers/dhcpdump/BasicDHCPDumpParserTest.java
+++ metron-platform/metron-parsers/src/test/java/org/apache/metron/parsers/dhcpdump/BasicDHCPDumpParserTest.java
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.metron.parsers.dhcpdump;
 
 import junit.framework.TestCase;
@@ -40,23 +39,40 @@ public class BasicDHCPDumpParserTest extends TestCase {
      * @throws Exception
      */
     public BasicDHCPDumpParserTest() throws Exception {
-        DHCPDumpParser = new BasicDHCPDumpParser();
+      DHCPDumpParser = new BasicDHCPDumpParser();
     }
 
     @Test
-    public void testSimpleMessage() throws ParseException, IOException, java.text.ParseException {
-        String rawMessage = "TIME: 2017-01-16 17:39:36.581|INTERFACE: eth2|OP:1 BOOTPREQUEST|CIADDR: 172.20.75.77|YIADDR: 0.0.0.0|SIADDR: 0.0.0.0|GIADDR: 172.20.75.8|CHADDR: fc:f8:ae:e8:ef:db:00:00:00:00:00:00:00:00:00:00|OPTION:  53   1 DHCP message type: 8 |DHCPINFORM|OPTION:  61   7 Client-identifier: 01:fc:f8:ae:e8:ef:db|OPTION:  12   5 Host name: A1244|OPTION:  60   8 Vendor class identifier: MSFT 5.0|OPTION:  55  13 Parameter Request List:   1 (Subnet mask)|| 15 (Domainname)||  3 (Routers)||  6 (DNS server)|| 44 (NetBIOS name server)|| 46 (NetBIOS node type)|| 47 (NetBIOS scope)|| 31 (Perform router discovery)|| 33 (Static route)||121 (Classless Static Route)||249 (MSFT - Classless route)|| 43 (Vendor specific info)||252 (MSFT - WinSock Proxy Auto Detect)|||IP: 100.100.100.177 > 172.20.1.11 | b8:ca:3a:67:95:8a > 0:50:56:84:68:43";
+    public void testBootprequestMessage() throws ParseException, IOException, java.text.ParseException {
+      String rawMessage = "TIME: 2017-04-21 15:43:17.673|IP: 172.20.3.28 (84:2b:2b:86:62:43) > 255.255.255.255 (ff:ff:ff:ff:ff:ff)|OP: 1 (BOOTPREQUEST)|HTYPE: 1 (Ethernet)|HLEN: 6|HOPS: 0|XID: 23850fd5|SECS: 0|FLAGS: 0|CIADDR: 172.20.3.28|YIADDR: 0.0.0.0|SIADDR: 0.0.0.0|GIADDR: 0.0.0.0|CHADDR: 84:2b:2b:86:62:43:00:00:00:00:00:00:00:00:00:00|SNAME: .|FNAME: .|OPTION:  53 (  1) DHCP message type         8 (DHCPINFORM)|OPTION:  61 (  7) Client-identifier         01:84:2b:2b:86:62:43|OPTION:  12 (  5) Host name                 Q0619|OPTION:  60 (  8) Vendor class identifier   MSFT 5.0|OPTION:  55 ( 13) Parameter Request List    1 (Subnet mask),15 (Domainname),3 (Routers),6 (DNS server),44 (NetBIOS name server),46 (NetBIOS node type),47 (NetBIOS scope),31 (Perform router discovery),33 (Static route),121 (Classless Static Route),249 (MSFT - Classless route),43 (Vendor specific info),252 (MSFT - WinSock Proxy Auto Detect)";
 
-        JSONObject metronJSONObject = DHCPDumpParser.parse(rawMessage.getBytes()).get(0);
+      JSONObject metronJSONObject = DHCPDumpParser.parse(rawMessage.getBytes()).get(0);
 
-        Assert.assertEquals(metronJSONObject.get("original_string").toString(), rawMessage);
-        Assert.assertEquals(new SimpleDateFormat(BasicDHCPDumpParser.TIMESTAMP_PATTERN).parse(metronJSONObject.get("time").toString()).getTime(), (long) metronJSONObject.get("timestamp"));
+      Assert.assertEquals(metronJSONObject.get("original_string").toString(), rawMessage);
+      Assert.assertEquals(new SimpleDateFormat(BasicDHCPDumpParser.TIMESTAMP_PATTERN).parse(metronJSONObject.get("time").toString()).getTime(), (long) metronJSONObject.get("timestamp"));
 
-        Assert.assertEquals(metronJSONObject.get("ip_src_addr").toString(), "100.100.100.177");
-        Assert.assertEquals(metronJSONObject.get("ip_dst_addr").toString(), "172.20.1.11");
+      Assert.assertEquals(metronJSONObject.get("ip_src_addr").toString(), "172.20.3.28");
+      Assert.assertEquals(metronJSONObject.get("ip_dst_addr").toString(), "255.255.255.255");
 
-        Assert.assertEquals(metronJSONObject.get("time").toString(), "2017-01-16 17:39:36.581");
+      Assert.assertEquals(metronJSONObject.get("host_name").toString(), "Q0619");
+      Assert.assertEquals(metronJSONObject.get("client_identifier").toString(), "01:84:2b:2b:86:62:43");
+
+      Assert.assertEquals(metronJSONObject.get("time").toString(), "2017-04-21 15:43:17.673");
     }
 
+    @Test
+    public void testBootpreplyMessage() throws ParseException, IOException, java.text.ParseException {
+      String rawMessage = "TIME: 2017-04-21 15:44:35.456|IP: 172.20.3.7 (20:fd:f1:71:6d:f1) > 255.255.255.255 (ff:ff:ff:ff:ff:ff)|OP: 2 (BOOTPREPLY)|HTYPE: 1 (Ethernet)|HLEN: 6|HOPS: 0|XID: cbc8934d|SECS: 0|FLAGS: 7f80|CIADDR: 172.20.3.56|YIADDR: 0.0.0.0|SIADDR: 0.0.0.0|GIADDR: 172.20.3.7|CHADDR: b8:ac:6f:c4:0f:88:00:00:00:00:00:00:00:00:00:00|SNAME: .|FNAME: .|OPTION:  53 (  1) DHCP message type         5 (DHCPACK)|OPTION:  54 (  4) Server identifier         172.20.1.10|OPTION:   1 (  4) Subnet mask               255.255.255.0|OPTION:  43 (  5) Vendor specific info      dc034e4150       ..NAP|OPTION:  15 ( 14) Domainname                oa.onsight.nl|OPTION:   3 (  4) Routers                   172.20.3.1|OPTION:   6 (  8) DNS server                172.20.1.10,172.20.1.11|OPTION:  44 (  8) NetBIOS name server       172.20.1.10,172.20.1.11|OPTION:  46 (  1) NetBIOS node type         8 (H-node)";
+
+      JSONObject metronJSONObject = DHCPDumpParser.parse(rawMessage.getBytes()).get(0);
+
+      Assert.assertEquals(metronJSONObject.get("original_string").toString(), rawMessage);
+      Assert.assertEquals(new SimpleDateFormat(BasicDHCPDumpParser.TIMESTAMP_PATTERN).parse(metronJSONObject.get("time").toString()).getTime(), (long) metronJSONObject.get("timestamp"));
+
+      Assert.assertEquals(metronJSONObject.get("ip_src_addr").toString(), "172.20.3.7");
+      Assert.assertEquals(metronJSONObject.get("ip_dst_addr").toString(), "255.255.255.255");
+
+      Assert.assertEquals(metronJSONObject.get("time").toString(), "2017-04-21 15:44:35.456");
+    }
 
 }
