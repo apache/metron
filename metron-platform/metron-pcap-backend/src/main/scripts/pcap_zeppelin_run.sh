0af6147a6c993ac1c4ceb41bc1bba97de0899116
cstella <cestella@gmail.com>
2017-05-01 09:31:19 -0400

    Adding license and such.

--- metron-platform/metron-pcap-backend/src/main/scripts/pcap_zeppelin_run.sh
+++ metron-platform/metron-pcap-backend/src/main/scripts/pcap_zeppelin_run.sh
@@ -1,5 +1,23 @@
 #!/bin/bash 
-METRON_HOME=${METRON_HOME:-"/usr/metron/0.4.0"}
+# 
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+
+METRON_HOME=${METRON_HOME:-"/usr/metron/${project.version}"}
 DATE_FORMAT=${DATE_FORMAT:-"yyyyMMdd"}
 USER=$(whoami)
 USER_HOMEDIR=${USER_HOMEDIR:-`hdfs getconf -confKey dfs.user.home.dir.prefix`/$USER}
