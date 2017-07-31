3053bcc7549768195f7302775bf0ee9537c71f01
Bas van de Lustgraaf <bas.van.de.lustgraaf@qsight.nl>
2017-07-31 15:13:00 +0200

    Add comment and restore unit test

--- metron-platform/metron-common/src/main/java/org/apache/metron/common/csv/CSVConverter.java
+++ metron-platform/metron-common/src/main/java/org/apache/metron/common/csv/CSVConverter.java
@@ -32,6 +32,13 @@ import java.util.Map;
 
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 
+/**
+ * Based on the parser config field `COLUMNS_KEY` the log line is
+ * transformed into a intermediate result using the CSV parser.
+ *
+ * All keys in `COLUMNS_KEY` and values extracted from the log line
+ * will be trimmed before adding to `values`.
+ */
 public class CSVConverter implements Serializable {
   public static final String COLUMNS_KEY="columns";
   public static final String SEPARATOR_KEY="separator";
