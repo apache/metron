14bcc0389431879b1d97ae53f8a8a6e7079bed46
jasper-k <jknulst@hortonworks.com>
2017-08-01 11:54:19 -0400

    METRON-1011 Stellar ENRICHMENT functions should enforce submission of 4 parameters (jasper-k via ottobackwards) closes apache/metron#628

--- metron-platform/metron-enrichment/src/test/java/org/apache/metron/enrichment/stellar/SimpleHBaseEnrichmentFunctionsTest.java
+++ metron-platform/metron-enrichment/src/test/java/org/apache/metron/enrichment/stellar/SimpleHBaseEnrichmentFunctionsTest.java
@@ -22,8 +22,10 @@ import com.google.common.collect.ImmutableMap;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.metron.stellar.dsl.Context;
+import org.apache.metron.stellar.dsl.ParseException;
 import org.apache.metron.stellar.dsl.StellarFunctions;
 import org.apache.metron.stellar.common.StellarProcessor;
+
 import org.apache.metron.enrichment.converter.EnrichmentHelper;
 import org.apache.metron.enrichment.converter.EnrichmentKey;
 import org.apache.metron.enrichment.converter.EnrichmentValue;
@@ -124,5 +126,11 @@ public class SimpleHBaseEnrichmentFunctionsTest {
     Assert.assertTrue(result instanceof Map);
     Map<String, Object> out = (Map<String, Object>) result;
     Assert.assertTrue(out.isEmpty());
+  }
+
+  @Test(expected = ParseException.class)
+  public void testProvidedParameters() throws Exception {
+    String stellar = "ENRICHMENT_GET('et', indicator)";
+    Object result = run(stellar, ImmutableMap.of("indicator", "indicator7"));
   }
 }
