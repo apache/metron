f34affdb25422f19f8e70a91b7a796a15a7745a5
merrimanr <merrimanr@gmail.com>
2017-08-24 15:32:23 -0500

    METRON-1073 Add option to limit fields returned from search query (merrimanr) closes apache/metron#677

--- metron-platform/metron-indexing/src/main/java/org/apache/metron/indexing/dao/search/SearchRequest.java
+++ metron-platform/metron-indexing/src/main/java/org/apache/metron/indexing/dao/search/SearchRequest.java
@@ -28,6 +28,7 @@ public class SearchRequest {
   private int size;
   private int from;
   private List<SortField> sort;
+  private List<String> fields;
   private List<String> facetFields;
 
   public SearchRequest() {
@@ -97,6 +98,14 @@ public class SearchRequest {
 
   public void setSort(List<SortField> sort) {
     this.sort = sort;
+  }
+
+  public Optional<List<String>> getFields() {
+    return fields == null || fields.size() == 0 ? Optional.empty() : Optional.of(fields);
+  }
+
+  public void setFields(List<String> fields) {
+    this.fields = fields;
   }
 
   public Optional<List<String>> getFacetFields() {
