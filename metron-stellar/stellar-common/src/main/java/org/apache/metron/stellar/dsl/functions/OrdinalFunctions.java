9410526274722f2ccdcded94a9ddadcf742defb4
cstella <cestella@gmail.com>
2017-11-29 15:52:16 -0500

    Updating functions and test.

--- metron-stellar/stellar-common/src/main/java/org/apache/metron/stellar/dsl/functions/OrdinalFunctions.java
+++ metron-stellar/stellar-common/src/main/java/org/apache/metron/stellar/dsl/functions/OrdinalFunctions.java
@@ -18,76 +18,91 @@
 
 package org.apache.metron.stellar.dsl.functions;
 
+import com.google.common.collect.Iterables;
+import org.apache.metron.stellar.common.utils.ConversionUtils;
 import org.apache.metron.stellar.dsl.BaseStellarFunction;
 import org.apache.metron.stellar.dsl.Stellar;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class OrdinalFunctions {
 
-    /**
-     * Stellar Function: MAX
-     * <p>
-     * Return the maximum value of a list of input values in a Stellar list
-     */
-    @Stellar(name = "MAX"
-            , description = "Returns the maximum value of a list of input values"
-            , params = {"list_of_values - Stellar list of values to evaluate. The list may only contain 1 type of object (only strings or only numbers)" +
-                        " and the objects must be comparable / ordinal"}
-            , returns = "The highest value in the list, null if the list is empty or the input values could not be ordered")
-    public static class Max extends BaseStellarFunction {
+  /**
+   * Stellar Function: MAX
+   * <p>
+   * Return the maximum value of a list of input values in a Stellar list
+   */
+  @Stellar(name = "MAX"
+          , description = "Returns the maximum value of a list of input values"
+          , params = {"list_of_values - Stellar list of values to evaluate. The list may only contain 1 type of object (only strings or only numbers)" +
+          " and the objects must be comparable / ordinal"}
+          , returns = "The highest value in the list, null if the list is empty or the input values could not be ordered")
+  public static class Max extends BaseStellarFunction {
 
-        @Override
-        public Object apply(List<Object> args) {
-            if (args.size() < 1 || args.get(0) == null) {
-                throw new IllegalStateException("MAX function requires at least a Stellar list of values");
-            }
-            List list = (List<Object>) args.get(0);
-            return orderList(list, true);
-        }
+    @Override
+    public Object apply(List<Object> args) {
+      if (args.size() < 1 || args.get(0) == null) {
+        throw new IllegalStateException("MAX function requires at least a Stellar list of values");
+      }
+      Iterable list = (Iterable<Object>) args.get(0);
+      return orderList(list, (ret, val) -> ret.compareTo(val) < 0, "MAX");
     }
+  }
 
-    /**
-     * Stellar Function: MIN
-     * <p>
-     * Return the minimum value of a list of input values in a Stellar list
-     */
-    @Stellar(name = "MIN"
-            , description = "Returns the minimum value of a list of input values"
-            , params = {"list_of_values - Stellar list of values to evaluate. The list may only contain 1 type of object (only strings or only numbers)" +
-            " and the objects must be comparable / ordinal"}
-            , returns = "The lowest value in the list, null if the list is empty or the input values could not be ordered")
-    public static class Min extends BaseStellarFunction {
-        @Override
-        public Object apply(List<Object> args) {
-            if (args.size() < 1 || args.get(0) == null) {
-                throw new IllegalStateException("MIN function requires at least a Stellar list of values");
-            }
-            List list = (List<Object>) args.get(0);
-            return orderList(list, false);
-        }
+  /**
+   * Stellar Function: MIN
+   * <p>
+   * Return the minimum value of a list of input values in a Stellar list
+   */
+  @Stellar(name = "MIN"
+          , description = "Returns the minimum value of a list of input values"
+          , params = {"list_of_values - Stellar list of values to evaluate. The list may only contain 1 type of object (only strings or only numbers)" +
+          " and the objects must be comparable / ordinal"}
+          , returns = "The lowest value in the list, null if the list is empty or the input values could not be ordered")
+  public static class Min extends BaseStellarFunction {
+    @Override
+    public Object apply(List<Object> args) {
+      if (args.size() < 1 || args.get(0) == null) {
+        throw new IllegalStateException("MIN function requires at least a Stellar list of values");
+      }
+      List<Comparable> list = (List<Comparable>) args.get(0);
+      return orderList(list, (ret, val) -> ret.compareTo(val) > 0, "MIN");
     }
+  }
 
-    private static Object orderList(List<Object> list, Boolean max) {
-        if (list.isEmpty()) {
-            return null;
-        }
-        List filteredList = (List<Object>) list.stream().filter(index -> !(index == null)).collect(Collectors.toList());
-        if (filteredList.isEmpty()) {
-            return null;
-        }
-        try {
-            if (max) {
-                Collections.sort(filteredList,Collections.reverseOrder());
-            }
-            else {
-                Collections.sort(filteredList);
-            }
-        } catch (ClassCastException e) {
-            throw new IllegalStateException("Mixed objects were submitted to MAX/MIN function. The Stellar list can only contain comparable objects of 1 type");
-        }
-        return filteredList.get(0);
+  private static Comparable orderList(Iterable<Comparable> list, BiFunction<Comparable, Comparable, Boolean> eval, String funcName) {
+    if (Iterables.isEmpty(list)) {
+      return null;
     }
+    Comparable ret = null;
+    Class<? extends Comparable> clazz = null;
+    for(Comparable<?> value : list) {
+      if(value == null) {
+        continue;
+      }
+      if(clazz == null) {
+        clazz = value.getClass();
+      }
+      try {
+        Comparable v = ConversionUtils.convert(value, clazz);
+        if(v == null && value != null) {
+          throw new IllegalStateException("Incomparable objects were submitted to " + funcName + ": "
+                  + clazz.getName() + " != " + value.getClass().getName());
+        }
+        if(ret == null || eval.apply(ret, v) ) {
+          ret = v;
+        }
+      }
+      catch(ClassCastException cce) {
+        throw new IllegalStateException("Incomparable objects were submitted to " + funcName
+                + ": " + cce.getMessage(), cce);
+      }
+    }
+    return ret;
+  }
+
 }
--- metron-stellar/stellar-common/src/test/java/org/apache/metron/stellar/dsl/functions/OrdinalFunctionsTest.java
+++ metron-stellar/stellar-common/src/test/java/org/apache/metron/stellar/dsl/functions/OrdinalFunctionsTest.java
@@ -36,125 +36,153 @@ import java.util.Map;
 
 public class OrdinalFunctionsTest {
 
-    private static Context context;
-    private static String INPUTLIST;
+  private static Context context;
+  private static String INPUTLIST;
 
-    @Before
-    public void setup() throws Exception {
-        context = new Context.Builder().build();
-    }
+  @Before
+  public void setup() throws Exception {
+    context = new Context.Builder().build();
+  }
 
-    @Test
-    public void testMaxOfStringList() throws Exception {
+  @Test
+  public void testMaxOfStringList() throws Exception {
 
-        List<Object> inputList = new ArrayList<Object>(){{
-            add("value3");
-            add("value1");
-            add("23");
-            add("value2");
-        }};
+    List<Object> inputList = new ArrayList<Object>(){{
+      add("value3");
+      add("value1");
+      add("23");
+      add("value2");
+    }};
 
-        Object res = run("MAX(input_list)", ImmutableMap.of("input_list", inputList));
-        Assert.assertNotNull(res);
-        Assert.assertTrue(res.equals("value3"));
-    }
+    Object res = run("MAX(input_list)", ImmutableMap.of("input_list", inputList));
+    Assert.assertNotNull(res);
+    Assert.assertTrue(res.equals("value3"));
+  }
 
-    @Test
-    public void testMaxOfIntegerList() throws Exception {
+  @Test
+  public void testMaxOfIntegerList() throws Exception {
 
-        List<Object> inputList = new ArrayList<Object>(){{
-            add(12);
-            add(56);
-        }};
+    List<Object> inputList = new ArrayList<Object>(){{
+      add(12);
+      add(56);
+    }};
 
-        Object res = run("MAX(input_list)", ImmutableMap.of("input_list", inputList));
-        Assert.assertNotNull(res);
-        Assert.assertTrue(res.equals(56));
-    }
+    Object res = run("MAX(input_list)", ImmutableMap.of("input_list", inputList));
+    Assert.assertNotNull(res);
+    Assert.assertTrue(res.equals(56));
+  }
 
-    @Test
-    public void testMaxWithVarList() throws Exception {
+  @Test
+  public void testMaxWithVarList() throws Exception {
 
-        List<Object> inputList = new ArrayList<Object>(){{
-            add(12);
-            add(56);
-        }};
+    List<Object> inputList = new ArrayList<Object>(){{
+      add(12);
+      add(56);
+    }};
 
-        Object res = run("MAX([string1,string2])", ImmutableMap.of("string1","abc","string2","def"));
-        Assert.assertNotNull(res);
-        Assert.assertTrue(res.equals("def"));
-    }
+    Object res = run("MAX([string1,string2])", ImmutableMap.of("string1","abc","string2","def"));
+    Assert.assertNotNull(res);
+    Assert.assertTrue(res.equals("def"));
+  }
 
-    @Test
-    public void testMinWithNullInList() throws Exception {
+  @Test
+  public void testMinWithNullInList() throws Exception {
 
-        List<Object> inputList = new ArrayList<Object>(){{
-            add(145);
-            add(null);
-        }};
+    List<Object> inputList = new ArrayList<Object>(){{
+      add(145);
+      add(null);
+    }};
 
-        Object res = run("MIN(input_list)", ImmutableMap.of("input_list", inputList));
-        Assert.assertNotNull(res);
-        Assert.assertTrue(res.equals(145));
-    }
+    Object res = run("MIN(input_list)", ImmutableMap.of("input_list", inputList));
+    Assert.assertNotNull(res);
+    Assert.assertTrue(res.equals(145));
+  }
 
-    @Test
-    public void testAllNullList() throws Exception {
+  @Test
+  public void testAllNullList() throws Exception {
 
-        List<Object> inputList = new ArrayList<Object>(){{
-            add(null);
-            add(null);
-        }};
+    List<Object> inputList = new ArrayList<Object>(){{
+      add(null);
+      add(null);
+    }};
 
-        Object res = run("MAX(input_list)", ImmutableMap.of("input_list", inputList));
-        Assert.assertNull(res);
-    }
+    Object res = run("MAX(input_list)", ImmutableMap.of("input_list", inputList));
+    Assert.assertNull(res);
+  }
 
-    @Test
-    public void testMinOfIntegerList() throws Exception {
+  @Test
+  public void testMinOfIntegerList() throws Exception {
 
-        List<Object> inputList = new ArrayList<Object>(){{
-            add(56);
-            add(12);
-            add(23);
-            add(null);
-        }};
+    List<Object> inputList = new ArrayList<Object>(){{
+      add(56);
+      add(12);
+      add(23);
+      add(null);
+    }};
 
-        Object res = run("MIN(input_list)", ImmutableMap.of("input_list", inputList));
-        Assert.assertNotNull(res);
-        Assert.assertTrue(res.equals(12));
-    }
+    Object res = run("MIN(input_list)", ImmutableMap.of("input_list", inputList));
+    Assert.assertNotNull(res);
+    Assert.assertTrue(res.equals(12));
+  }
 
 
-    @Test
-    public void testMaxOfLongList() throws Exception {
+  @Test
+  public void testMaxOfLongList() throws Exception {
 
-        List<Object> inputList = new ArrayList<Object>(){{
-            add(12L);
-            add(56L);
-            add(457L);
-        }};
+    List<Object> inputList = new ArrayList<Object>(){{
+      add(12L);
+      add(56L);
+      add(457L);
+    }};
 
-        Object res = run("MAX(input_list)", ImmutableMap.of("input_list", inputList));
-        Assert.assertNotNull(res);
-        Assert.assertTrue(res.equals(457L));
-    }
+    Object res = run("MAX(input_list)", ImmutableMap.of("input_list", inputList));
+    Assert.assertNotNull(res);
+    Assert.assertTrue(res.equals(457L));
+  }
 
-    @Test(expected = IllegalStateException.class)
-    public void testMaxOfMixedList() throws Exception {
+  @Test
+  public void testMaxOfMixedNumerical() throws Exception {
 
-        List<Object> inputList = new ArrayList<Object>(){{
-            add(12);
-            add("string");
-            add(457L);
-        }};
+    List<Object> inputList = new ArrayList<Object>(){{
+      add(12L);
+      add(56.0);
+      add(457L);
+    }};
 
-        Object res = run("MAX(input_list)", ImmutableMap.of("input_list", inputList));
-    }
+    Object res = run("MAX(input_list)", ImmutableMap.of("input_list", inputList));
+    Assert.assertNotNull(res);
+    Assert.assertTrue(res.equals(457L));
+  }
 
-    public Object run(String rule, Map<String, Object> variables) throws Exception {
-        StellarProcessor processor = new StellarProcessor();
-        return processor.parse(rule, new DefaultVariableResolver(x -> variables.get(x), x -> variables.containsKey(x)), StellarFunctions.FUNCTION_RESOLVER(), context);
-    }
+  @Test
+  public void testMinOfMixedNumerical() throws Exception {
+
+    List<Object> inputList = new ArrayList<Object>(){{
+      add(12L);
+      add(56.0);
+      add(457L);
+    }};
+
+    Object res = run("MIN(input_list)", ImmutableMap.of("input_list", inputList));
+    Assert.assertNotNull(res);
+    Assert.assertTrue(res.equals(12L));
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void testMaxOfMixedList() throws Exception {
+
+    List<Object> inputList = new ArrayList<Object>(){{
+      add(12);
+      add("string");
+      add(457L);
+    }};
+
+    Object res = run("MAX(input_list)", ImmutableMap.of("input_list", inputList));
+  }
+
+  public Object run(String rule, Map<String, Object> variables) throws Exception {
+    StellarProcessor processor = new StellarProcessor();
+    return processor.parse(rule, new DefaultVariableResolver(x -> variables.get(x), x -> variables.containsKey(x)), StellarFunctions.FUNCTION_RESOLVER(), context);
+  }
 
 }
