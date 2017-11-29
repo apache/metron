3a95d2ac44815d6e603524a3b99787ca969133e6
jknulst <jknulst@hortonworks.com>
2017-11-29 22:55:41 +0100

    Added test for Set input

--- metron-stellar/stellar-common/src/test/java/org/apache/metron/stellar/dsl/functions/OrdinalFunctionsTest.java
+++ metron-stellar/stellar-common/src/test/java/org/apache/metron/stellar/dsl/functions/OrdinalFunctionsTest.java
@@ -31,9 +31,7 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class OrdinalFunctionsTest {
@@ -102,11 +100,6 @@ public class OrdinalFunctionsTest {
 
   @Test
   public void testMaxWithVarList() throws Exception {
-
-    List<Object> inputList = new ArrayList<Object>(){{
-      add(12);
-      add(56);
-    }};
 
     Object res = run("MAX([string1,string2])", ImmutableMap.of("string1","abc","string2","def"));
     Assert.assertNotNull(res);
@@ -185,6 +178,20 @@ public class OrdinalFunctionsTest {
       Assert.assertThat(e.getMessage(), is("Incomparable objects were submitted to MAX: class java.lang.String is incomparable to class java.lang.Long"));
       Assert.assertNull(res);
     }
+  }
+
+  @Test
+  public void testSetInput() throws Exception {
+
+    Set<Object> inputSet = new HashSet<Object>(){{
+      add(14L);
+      add(15.3d);
+      add(15);
+    }};
+
+    Object res = run("MAX(input_set)", ImmutableMap.of("input_set", inputSet));
+    Assert.assertNotNull(res);
+    Assert.assertTrue(res.equals(15.3d));
   }
 
   @Test
