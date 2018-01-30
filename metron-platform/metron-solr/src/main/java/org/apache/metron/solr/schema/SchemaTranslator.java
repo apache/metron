ef0d6933d39c636cebefa51930e442730d7c59cf
cstella <cestella@gmail.com>
2018-01-30 18:51:23 -0500

    Updating to include the schemas.

--- metron-platform/metron-solr/src/main/java/org/apache/metron/solr/schema/SchemaTranslator.java
+++ metron-platform/metron-solr/src/main/java/org/apache/metron/solr/schema/SchemaTranslator.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.metron.solr.schema;
 
 import com.google.common.collect.ImmutableSet;
@@ -118,7 +135,11 @@ public class SchemaTranslator {
       for(Map.Entry<String, Object> dynamicFieldDef : dynamicProperty.entrySet()) {
         System.out.println("Processing dynamic property: " + dynamicFieldDef.getKey());
         Map<String, Object> def = (Map<String, Object>) dynamicFieldDef.getValue();
-        String match = normalizeField((String) def.get("match"));
+        String match = (String) def.get("match");
+        if(match == null) {
+          match = (String) def.get("path_match");
+        }
+        match = normalizeField(match);
         String type = (String)((Map<String, Object>)def.get("mapping")).get("type");
         SolrFields solrField = SolrFields.byElasticsearchType(type);
         if(solrField == null) {
