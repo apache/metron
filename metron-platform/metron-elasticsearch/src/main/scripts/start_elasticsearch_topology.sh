2a6e406350718a0fef177a26c2cdf853e10c28ff
Michael Miklavcic <michael.miklavcic@gmail.com>
2018-03-08 23:43:09 -0700

    topology start script fix

--- metron-platform/metron-elasticsearch/src/main/scripts/start_elasticsearch_topology.sh
+++ metron-platform/metron-elasticsearch/src/main/scripts/start_elasticsearch_topology.sh
@@ -21,8 +21,8 @@ METRON_HOME=/usr/metron/$METRON_VERSION
 TOPOLOGY_JAR=${project.artifactId}-$METRON_VERSION-uber.jar
 INDEXING_CONTRIB=${INDEXING_CONTRIB:-$METRON_HOME/indexing_contrib}
 if [ -d "$INDEXING_CONTRIB" ]; then
-  export EXTRA_JARS="$INDEXING_CONTRIB/*"
-  storm jar $METRON_HOME/lib/$TOPOLOGY_JAR org.apache.storm.flux.Flux --remote $METRON_HOME/flux/indexing/random_access/remote.yaml --filter $METRON_HOME/config/elasticsearch.properties --jars "$INDEXING_CONTRIB"
+  export EXTRA_JARS=$(ls -m $INDEXING_CONTRIB/*.jar | tr -d ' ' | tr -d '\n' | sed 's/\/\//\//g')
+  storm jar $METRON_HOME/lib/$TOPOLOGY_JAR org.apache.storm.flux.Flux --remote $METRON_HOME/flux/indexing/random_access/remote.yaml --filter $METRON_HOME/config/elasticsearch.properties --jars "$EXTRA_JARS"
 else
   storm jar $METRON_HOME/lib/$TOPOLOGY_JAR org.apache.storm.flux.Flux --remote $METRON_HOME/flux/indexing/random_access/remote.yaml --filter $METRON_HOME/config/elasticsearch.properties
 fi
