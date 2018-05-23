e25031905a3c85aeadd2063486abaa38c94a1eb4
merrimanr <merrimanr@gmail.com>
2018-05-23 08:57:19 -0500

    METRON-1566 Alert updates are not propagated to metaalert child alerts (merrimanr) closes apache/metron#1018

--- metron-platform/metron-indexing/src/main/java/org/apache/metron/indexing/dao/HBaseDao.java
+++ metron-platform/metron-indexing/src/main/java/org/apache/metron/indexing/dao/HBaseDao.java
@@ -250,7 +250,7 @@ public class HBaseDao implements IndexDao {
   protected Put buildPut(Document update) throws IOException {
     Key k = new Key(update.getGuid(), update.getSensorType());
     Put put = new Put(Key.toBytes(k));
-    long ts = update.getTimestamp() == null ? System.currentTimeMillis() : update.getTimestamp();
+    long ts = update.getTimestamp() == null || update.getTimestamp() == 0 ? System.currentTimeMillis() : update.getTimestamp();
     byte[] columnQualifier = Bytes.toBytes(ts);
     byte[] doc = JSONUtils.INSTANCE.toJSONPretty(update.getDocument());
     put.addColumn(cf, columnQualifier, doc);
