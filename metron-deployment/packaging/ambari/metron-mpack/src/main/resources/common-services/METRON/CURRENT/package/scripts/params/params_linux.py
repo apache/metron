c2d102340c84fe3b5a83cdec2ebc2692b0149c97
Nick Allen <nick@nickallen.org>
2018-06-18 12:29:14 -0400

    Moved source type field to live under Metron REST settings

--- metron-deployment/packaging/ambari/metron-mpack/src/main/resources/common-services/METRON/CURRENT/package/scripts/params/params_linux.py
+++ metron-deployment/packaging/ambari/metron-mpack/src/main/resources/common-services/METRON/CURRENT/package/scripts/params/params_linux.py
@@ -49,7 +49,6 @@ metron_user = status_params.metron_user
 metron_group = config['configurations']['metron-env']['metron_group']
 metron_log_dir = config['configurations']['metron-env']['metron_log_dir']
 metron_pid_dir = config['configurations']['metron-env']['metron_pid_dir']
-source_type_field = config['configurations']['metron-env']['source_type_field']
 
 metron_rest_port = status_params.metron_rest_port
 metron_management_ui_host = status_params.metron_management_ui_host
@@ -255,6 +254,7 @@ metron_rest_classpath = config['configurations']['metron-rest-env']['metron_rest
 metron_sysconfig = config['configurations']['metron-rest-env']['metron_sysconfig']
 user_settings_hbase_table = status_params.user_settings_hbase_table
 user_settings_hbase_cf = status_params.user_settings_hbase_cf
+source_type_field = config['configurations']['metron-rest-env']['source_type_field']
 
 # Enrichment
 metron_enrichment_topology = status_params.metron_enrichment_topology
