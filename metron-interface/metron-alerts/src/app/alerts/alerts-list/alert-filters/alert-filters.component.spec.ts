fc9ff85368c2a974d30ad6ccdb73a4cc26fd025c
tiborm <tibor.meller@gmail.com>
2018-07-02 11:07:28 -0400

    METRON-1636: Fix broken unit test setup in metron-alerts closes apache/incubator-metron#1085

--- metron-interface/metron-alerts/src/app/alerts/alerts-list/alert-filters/alert-filters.component.spec.ts
+++ metron-interface/metron-alerts/src/app/alerts/alerts-list/alert-filters/alert-filters.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { CollapseComponent } from '../../../shared/collapse/collapse.component';
 import { AlertFiltersComponent } from './alert-filters.component';
+import { CenterEllipsesPipe } from '../../../shared/pipes/center-ellipses.pipe';
+import { ColumnNameTranslatePipe } from '../../../shared/pipes/column-name-translate.pipe';
 
 describe('AlertFiltersComponent', () => {
   let component: AlertFiltersComponent;
@@ -8,16 +11,19 @@ describe('AlertFiltersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AlertFiltersComponent ]
+      declarations: [ 
+        CollapseComponent,
+        CenterEllipsesPipe,
+        ColumnNameTranslatePipe,
+        AlertFiltersComponent,
+      ]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(AlertFiltersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should be created', () => {
     expect(component).toBeTruthy();
--- metron-interface/metron-alerts/src/app/shared/directives/alert-severity-hexagon.directive.spec.ts
+++ metron-interface/metron-alerts/src/app/shared/directives/alert-severity-hexagon.directive.spec.ts
@@ -1,8 +1,9 @@
+import { ElementRef } from '@angular/core';
 import { AlertSeverityHexagonDirective } from './alert-severity-hexagon.directive';
 
 describe('AlertSeverityHexagonDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AlertSeverityHexagonDirective();
+  it('should be created', () => {
+    const directive = new AlertSeverityHexagonDirective(new ElementRef({}));
     expect(directive).toBeTruthy();
   });
 });
