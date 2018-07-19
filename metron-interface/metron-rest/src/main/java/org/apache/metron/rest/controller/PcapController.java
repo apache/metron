eb0e1a72c2dc374f4f1a29e1e2bb24474fb8f6d5
merrimanr <merrimanr@gmail.com>
2018-07-19 13:45:24 -0500

    added documentation

--- metron-interface/metron-rest/src/main/java/org/apache/metron/rest/controller/PcapController.java
+++ metron-interface/metron-rest/src/main/java/org/apache/metron/rest/controller/PcapController.java
@@ -55,16 +55,18 @@ public class PcapController {
   @ApiResponses(value = { @ApiResponse(message = "Returns a job status with job ID.", code = 200)})
   @RequestMapping(value = "/fixed", method = RequestMethod.POST)
   ResponseEntity<PcapStatus> fixed(@ApiParam(name="fixedPcapRequest", value="A Fixed Pcap Request"
-          + " which includes fixed filter fields like ip source address and protocol.", required=true)@RequestBody FixedPcapRequest fixedPcapRequest) throws RestException {
+          + " which includes fixed filter fields like ip source address and protocol", required=true)@RequestBody FixedPcapRequest fixedPcapRequest) throws RestException {
     PcapStatus pcapStatus = pcapQueryService.fixed(SecurityUtils.getCurrentUser(), fixedPcapRequest);
     return new ResponseEntity<>(pcapStatus, HttpStatus.OK);
   }
 
-  @ApiOperation(value = "Gets job status for running job.")
-  @ApiResponses(value = { @ApiResponse(message = "Returns a job status for the passed job.", code = 200)})
+  @ApiOperation(value = "Gets job status for Pcap query job.")
+  @ApiResponses(value = {
+          @ApiResponse(message = "Returns a job status for the Job ID.", code = 200),
+          @ApiResponse(message = "Job is missing.", code = 404)
+  })
   @RequestMapping(value = "/{jobId}", method = RequestMethod.GET)
-  ResponseEntity<PcapStatus> getStatus(@ApiParam(name="jobId", value="Job ID of submitted job"
-      + " which includes fixed filter fields like ip source address and protocol.", required=true)@PathVariable String jobId) throws RestException {
+  ResponseEntity<PcapStatus> getStatus(@ApiParam(name="jobId", value="Job ID of submitted job", required=true)@PathVariable String jobId) throws RestException {
     PcapStatus jobStatus = pcapQueryService.getJobStatus(SecurityUtils.getCurrentUser(), jobId);
     if (jobStatus != null) {
       return new ResponseEntity<>(jobStatus, HttpStatus.OK);
@@ -73,8 +75,14 @@ public class PcapController {
     }
   }
 
+  @ApiOperation(value = "Gets Pcap Results for a page in PDML format.")
+  @ApiResponses(value = {
+          @ApiResponse(message = "Returns PDML in json format.", code = 200),
+          @ApiResponse(message = "Job or page is missing.", code = 404)
+  })
   @RequestMapping(value = "/{jobId}/pdml", method = RequestMethod.GET)
-  ResponseEntity<Pdml> pdml(@PathVariable String jobId, @ApiParam(name="page", value="Path to pcap result page", required=true)@RequestParam Integer page) throws RestException {
+  ResponseEntity<Pdml> pdml(@ApiParam(name="jobId", value="Job ID of submitted job", required=true)@PathVariable String jobId,
+                            @ApiParam(name="page", value="Page number", required=true)@RequestParam Integer page) throws RestException {
     Pdml pdml = pcapQueryService.getPdml(SecurityUtils.getCurrentUser(), jobId, page);
     if (pdml != null) {
       return new ResponseEntity<>(pdml, HttpStatus.OK);
