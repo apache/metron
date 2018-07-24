8a19ac5ba4fd0a9b3cfd66fcd2d1f8d25059ce73
Shane Ardell <sardell@hortonworks.com>
2018-07-24 13:24:51 +0200

    remove unused BehaviorSubject from pcap service

--- metron-interface/metron-alerts/src/app/pcap/service/pcap.service.ts
+++ metron-interface/metron-alerts/src/app/pcap/service/pcap.service.ts
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 import {Injectable, NgZone} from '@angular/core';
-import {Observable, BehaviorSubject} from 'rxjs/Rx';
+import {Observable} from 'rxjs/Rx';
 import {Http, Headers, RequestOptions} from '@angular/http';
 import {HttpUtil} from '../../utils/httpUtil';
 
@@ -24,7 +24,6 @@ import 'rxjs/add/operator/map';
 
 import {PcapRequest} from '../model/pcap.request';
 import {Pdml} from '../model/pdml';
-import { RestError } from '../../model/rest-error';
 
 export class PcapStatusResponse {
     jobStatus: string;
@@ -34,8 +33,6 @@ export class PcapStatusResponse {
 
 @Injectable()
 export class PcapService {
-
-    pdml: BehaviorSubject<Pdml>;
 
     private statusInterval = 4;
     defaultHeaders = {'Content-Type': 'application/json', 'X-Requested-With': 'XMLHttpRequest'};
@@ -70,7 +67,7 @@ export class PcapService {
 
     public getPackets(id: string, pageId: number): Observable<Pdml> {
         return this.http.get(`/api/v1/pcap/${id}/pdml?page=${pageId}`, new RequestOptions({headers: new Headers(this.defaultHeaders)}))
-            .map(res => this.pdml = HttpUtil.extractData(res))
+            .map(HttpUtil.extractData)
             .catch(HttpUtil.handleError)
             .onErrorResumeNext();
     }
