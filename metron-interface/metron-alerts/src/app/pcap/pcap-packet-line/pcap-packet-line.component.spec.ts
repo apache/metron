a51768216f86a3977da8426f1719ce409774186c
tiborm <tibor.meller@gmail.com>
2018-07-30 14:44:36 +0200

    refactoring out test data and sharing across multiple test suite

--- metron-interface/metron-alerts/src/app/pcap/pcap-packet-line/pcap-packet-line.component.spec.ts
+++ metron-interface/metron-alerts/src/app/pcap/pcap-packet-line/pcap-packet-line.component.spec.ts
@@ -16,40 +16,9 @@
  * limitations under the License.
  */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { fakePacket } from '../model/pdml.mock';
 
 import { PcapPacketLineComponent } from './pcap-packet-line.component';
-import { PdmlPacket } from '../model/pdml';
-
-const fakePacket = {
-  "name": '',
-  "expanded": false,
-  "protos": [
-    {
-      "name": "geninfo",
-      "showname": "",
-      "fields": [
-        { "name": "timestamp", "pos": "0", "showname": "Captured Time", "size": "722", "value": "1458240269.373968000", "show": "Mar 17, 2016 18:44:29.373968000 UTC", "unmaskedvalue": null, "hide": null, "fields": null, "protos": null }
-      ]
-    },
-    {
-      "name": "ip",
-      "showname": "",
-      "fields": [
-        { "name": "ip.proto", "pos": "23", "showname": "Protocol: TCP (6)", "size": "1", "value": "06", "show": "6", "unmaskedvalue": null, "hide": null, "fields": null, "protos": null },
-        { "name": "ip.src", "pos": "26", "showname": "Source: 192.168.66.121 (192.168.66.121)", "size": "4", "value": "c0a84279", "show": "192.168.66.121", "unmaskedvalue": null, "hide": null, "fields": null, "protos": null },
-        { "name": "ip.dst", "pos": "30", "showname": "Destination: 192.168.66.1 (192.168.66.1)", "size": "4", "value": "c0a84201", "show": "192.168.66.1", "unmaskedvalue": null, "hide": null, "fields": null, "protos": null }
-      ]
-    },
-    {
-      "name": "tcp",
-      "showname": "",
-      "fields": [
-        { "name": "tcp.srcport", "pos": "34", "showname": "Source port: ssh (22)", "size": "2", "value": "0016", "show": "22", "unmaskedvalue": null, "hide": null, "fields": null, "protos": null },
-        { "name": "tcp.dstport", "pos": "36", "showname": "Destination port: 55791 (55791)", "size": "2", "value": "d9ef", "show": "55791", "unmaskedvalue": null, "hide": null, "fields": null, "protos": null }
-      ],
-    },
-  ]
-};
 
 describe('PcapPacketLineComponent', () => {
   let component: PcapPacketLineComponent;
@@ -65,7 +34,7 @@ describe('PcapPacketLineComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PcapPacketLineComponent);
     component = fixture.componentInstance;
-    component.packet = fakePacket as PdmlPacket;
+    component.packet = fakePacket;
     fixture.detectChanges();
   });
 
