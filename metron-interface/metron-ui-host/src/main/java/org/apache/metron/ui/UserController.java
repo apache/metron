6400a50730dcbfbcb9e52674377d8c6323c8573b
Simon Elliston Ball <simon@simonellistonball.com>
2018-07-31 23:34:47 +0100

    Cleaned up imports

--- metron-interface/metron-ui-host/src/main/java/org/apache/metron/ui/UserController.java
+++ metron-interface/metron-ui-host/src/main/java/org/apache/metron/ui/UserController.java
@@ -34,7 +34,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 /**
