12c94d4a940326d15d22f79b95223ed00f532dc3
ruffle1986 <ftamas.mail@gmail.com>
2018-08-24 12:12:45 +0200

    fix(pcap-filters): validate start date if end date changes and vica versa.

--- metron-interface/metron-alerts/src/app/pcap/pcap-filters/pcap-filters.component.ts
+++ metron-interface/metron-alerts/src/app/pcap/pcap-filters/pcap-filters.component.ts
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {Component, Input, Output, EventEmitter, OnChanges, SimpleChanges} from '@angular/core';
+import {Component, Input, Output, EventEmitter, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import { FormGroup, FormControl, Validators, ValidationErrors } from '@angular/forms';
 
 import * as moment from 'moment/moment';
@@ -23,17 +23,27 @@ import { DEFAULT_START_TIME, DEFAULT_END_TIME, DEFAULT_TIMESTAMP_FORMAT } from '
 
 import { PcapRequest } from '../model/pcap.request';
 
-function dateRangeValidator(formControl: FormControl): ValidationErrors | null {
+function validateStartDate(formControl: FormControl): ValidationErrors | null {
   if (!formControl.parent) {
     return null;
   }
-
   const filterForm = formControl.parent;
   const startTimeMs = new Date(filterForm.controls['startTime'].value).getTime();
   const endTimeMs = new Date(filterForm.controls['endTime'].value).getTime();
+  if (startTimeMs > endTimeMs) {
+    return { error: 'Start time cannot be bigger than end date.' };
+  }
+  return null;
+}
 
-  if (startTimeMs > endTimeMs || endTimeMs > new Date().getTime()) {
-    return { error: 'Selected date range is invalid.' };
+function validateEndDate(formControl: FormControl): ValidationErrors | null {
+  if (!formControl.parent) {
+    return null;
+  }
+  const filterForm = formControl.parent;
+  const endTimeMs = new Date(filterForm.controls['endTime'].value).getTime();
+  if (endTimeMs > new Date().getTime()) {
+    return { error: 'End time cannot be in the future.' };
   }
   return null;
 }
@@ -91,7 +101,7 @@ export type PcapFilterFormValue = {
   templateUrl: './pcap-filters.component.html',
   styleUrls: ['./pcap-filters.component.scss']
 })
-export class PcapFiltersComponent implements OnChanges {
+export class PcapFiltersComponent implements OnInit, OnChanges {
 
   @Input() queryRunning = true;
   @Input() model: PcapRequest = new PcapRequest();
@@ -101,8 +111,8 @@ export class PcapFiltersComponent implements OnChanges {
   private validPort: RegExp = /^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/;
 
   filterForm = new FormGroup({
-    startTime: new FormControl(moment(DEFAULT_START_TIME).format(DEFAULT_TIMESTAMP_FORMAT), dateRangeValidator),
-    endTime: new FormControl(moment(DEFAULT_END_TIME).format(DEFAULT_TIMESTAMP_FORMAT), dateRangeValidator),
+    startTime: new FormControl(moment(DEFAULT_START_TIME).format(DEFAULT_TIMESTAMP_FORMAT), validateStartDate),
+    endTime: new FormControl(moment(DEFAULT_END_TIME).format(DEFAULT_TIMESTAMP_FORMAT), validateEndDate),
     ipSrcAddr: new FormControl('', Validators.pattern(this.validIp)),
     ipSrcPort: new FormControl('', Validators.pattern(this.validPort)),
     ipDstAddr: new FormControl('', Validators.pattern(this.validIp)),
@@ -111,6 +121,19 @@ export class PcapFiltersComponent implements OnChanges {
     includeReverse: new FormControl(),
     packetFilter: new FormControl(''),
   });
+
+  ngOnInit() {
+    this.filterForm.get('startTime').valueChanges.subscribe((value) => {
+       this.filterForm.get('endTime').updateValueAndValidity({
+         emitEvent: false
+       });
+    });
+    this.filterForm.get('endTime').valueChanges.subscribe((value) => {
+      this.filterForm.get('startTime').updateValueAndValidity({
+        emitEvent: false
+      });
+    });
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['model']) {
