a3b43b021a1ce5ac8827791edaa3115cc4f21127
Simon Elliston Ball <simon@simonellistonball.com>
2018-08-28 17:28:39 +1200

    Fixed whitespace

--- metron-interface/metron-config-host/src/main/java/org/apache/metron/ui/ConfigApplication.java
+++ metron-interface/metron-config-host/src/main/java/org/apache/metron/ui/ConfigApplication.java
@@ -25,7 +25,7 @@ import org.springframework.context.annotation.ComponentScans;
 @SpringBootApplication
 @ComponentScans(value = { @ComponentScan, @ComponentScan(basePackageClasses = MetronSecurityConfig.class) })
 public class ConfigApplication extends AbstractHostApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigApplication.class, args);
+  }
 }
