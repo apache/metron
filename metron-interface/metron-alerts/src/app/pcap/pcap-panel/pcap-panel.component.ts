412d8b4ea1d320aa7960cedd98226944257a78b2
tiborm <tibor.meller@gmail.com>
2018-08-31 10:37:04 +0200

    removing missleading and unnecessary @Input annotations

--- metron-interface/metron-alerts/src/app/pcap/pcap-panel/pcap-panel.component.ts
+++ metron-interface/metron-alerts/src/app/pcap/pcap-panel/pcap-panel.component.ts
@@ -32,9 +32,9 @@ import { RestError } from '../../model/rest-error';
 })
 export class PcapPanelComponent implements OnInit, OnDestroy {
 
-  @Input() pdml: Pdml = null;
-  @Input() pcapRequest: PcapRequest;
-  @Input() resetPaginationForSearch: boolean;
+  pdml: Pdml = null;
+  pcapRequest: PcapRequest;
+  resetPaginationForSearch: boolean;
 
   statusSubscription: Subscription;
   cancelSubscription: Subscription;
