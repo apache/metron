1d927c975677424bc073838df6d906c3e3026bb5
merrimanr <merrimanr@gmail.com>
2018-09-12 12:14:11 -0500

    METRON-1756 REST tests should use Embedded LDAP in metron-security (merrimanr) closes apache/metron#1186

--- metron-interface/metron-rest/src/test/java/org/apache/metron/rest/controller/UserControllerIntegrationTest.java
+++ metron-interface/metron-rest/src/test/java/org/apache/metron/rest/controller/UserControllerIntegrationTest.java
@@ -29,6 +29,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.apache.metron.rest.MetronRestConstants.TEST_PROFILE;
+import static org.apache.metron.ui.EmbeddedLdap.EMBEDDED_LDAP_PROFILE;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -37,7 +38,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles(TEST_PROFILE)
+@ActiveProfiles({TEST_PROFILE, EMBEDDED_LDAP_PROFILE})
 public class UserControllerIntegrationTest {
 
   private MockMvc mockMvc;
