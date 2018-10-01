1723a0eae57adcfc4a2efe2422f52fbed4c237e7
JonZeolla <zeolla@gmail.com>
2018-10-01 08:41:25 -0400

    METRON-1758 Add support for Ansible 2.6 in dev (JonZeolla via jonzeolla) closes apache/metron#1179

--- metron-deployment/ansible/callback_plugins/0_minimum_ansible_version.py
+++ metron-deployment/ansible/callback_plugins/0_minimum_ansible_version.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #
 #  Licensed to the Apache Software Foundation (ASF) under one or more
 #  contributor license agreements.  See the NOTICE file distributed with
@@ -13,11 +14,32 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+#
 
-[defaults]
-host_key_checking = false
-library = extra_modules
-roles_path = ./roles
+import sys
+from ansible import __version__
 
-[ssh_connection]
-control_path = %(directory)s/%%h-%%p-%%r
+from ansible.plugins.callback import CallbackBase
+from ansible.utils.display import Display
+
+def display(*args, **kwargs):
+  display_instance = Display()
+  display_instance.display(*args, **kwargs)
+
+MINIMUM_ANSIBLE_VERSION = '2.4.0'
+
+def version_requirement(version):
+    return version >= MINIMUM_ANSIBLE_VERSION
+
+class CallbackModule(CallbackBase):
+  """
+  This enforces a minimum version of ansible
+  """
+
+  CALLBACK_NAME = 'minimum_ansible_version'
+
+  def __init__(self):
+    super(CallbackModule, self).__init__()
+    if not version_requirement(__version__):
+      display('Metron requires Ansible %s or newer, current version is %s' % (MINIMUM_ANSIBLE_VERSION, __version__), color='red')
+      sys.exit(1)
