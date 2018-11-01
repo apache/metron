056497034f7d0b5ebba7d8a66f1b6ff66ebfd119
Nick Allen <nick@nickallen.org>
2018-11-01 09:57:06 -0400

    METRON-1845 Added license header

--- metron-platform/metron-elasticsearch/src/test/java/org/apache/metron/elasticsearch/bulk/ElasticsearchBulkDocumentWriterTest.java
+++ metron-platform/metron-elasticsearch/src/test/java/org/apache/metron/elasticsearch/bulk/ElasticsearchBulkDocumentWriterTest.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.metron.elasticsearch.bulk;
 
 import org.apache.metron.common.Constants;
@@ -6,7 +23,6 @@ import org.apache.metron.indexing.dao.update.Document;
 import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
-import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.json.simple.JSONObject;
 import org.junit.Before;
@@ -22,10 +38,7 @@ import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -62,8 +75,8 @@ public class ElasticsearchBulkDocumentWriterTest {
 
         // validate the "on success" callback
         writer.onSuccess(successfulDocs -> {
-            onSuccessCalled = true;
             assertEquals(documents, successfulDocs);
+            onSuccessCalled = true;
         });
 
         writer.write(documents);
@@ -95,8 +108,8 @@ public class ElasticsearchBulkDocumentWriterTest {
 
         // validate the "on failure" callback
         writer.onFailure((failedDoc, cause, msg) -> {
-            onFailureCalled = true;
             assertEquals(documents.get(0), failedDoc);
+            onFailureCalled = true;
         });
 
         // no callbacks defined
@@ -133,8 +146,8 @@ public class ElasticsearchBulkDocumentWriterTest {
 
         // validate the "on success" callback
         writer.onSuccess(successfulDocs -> {
-            onSuccessCalled = true;
             assertEquals(documents, successfulDocs);
+            onSuccessCalled = true;
         });
 
         writer.write(documents);
