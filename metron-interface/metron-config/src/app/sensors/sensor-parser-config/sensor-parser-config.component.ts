b89f066663b651d11abdf302da034d863559ce4e
tiborm <tibor.meller@gmail.com>
2019-02-14 17:02:40 +0100

    METRON-1999 Adding validation against special characters to parser name field (tiborm via sardell) closes apache/metron#1337

--- metron-interface/metron-config/src/app/sensors/sensor-parser-config/sensor-parser-config.component.ts
+++ metron-interface/metron-config/src/app/sensors/sensor-parser-config/sensor-parser-config.component.ts
@@ -72,6 +72,7 @@ export class SensorParserConfigComponent implements OnInit {
   configValid = false;
   sensorNameValid = false;
   sensorNameUnique = true;
+  sensorNameNoSpecChars = false;
   kafkaTopicValid = false;
   parserClassValid = false;
   grokStatementValid = false;
@@ -296,10 +297,14 @@ export class SensorParserConfigComponent implements OnInit {
 
   onSetSensorName(): void {
     this.sensorNameUnique = this.currentSensors.indexOf(this.sensorName) === -1;
+    this.sensorNameNoSpecChars = !/[^a-zA-Z0-9_-]/.test(this.sensorName);
+
     this.sensorNameValid =
       this.sensorName !== undefined &&
       this.sensorName.length > 0 &&
-      this.sensorNameUnique;
+      this.sensorNameUnique &&
+      this.sensorNameNoSpecChars;
+
     this.isConfigValid();
   }
 
