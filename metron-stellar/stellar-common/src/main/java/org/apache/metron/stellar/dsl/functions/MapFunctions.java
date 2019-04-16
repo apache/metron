dc273a5be628c458ad0b25380d55536cd37b7a4f
mmiklavc <michael.miklavcic@gmail.com>
2019-04-16 12:37:16 -0600

    METRON-2071 Add MAP_PUT and MAP_MERGE to Stellar (mmiklavc) closes apache/metron#1385

--- metron-stellar/stellar-common/src/main/java/org/apache/metron/stellar/dsl/functions/MapFunctions.java
+++ metron-stellar/stellar-common/src/main/java/org/apache/metron/stellar/dsl/functions/MapFunctions.java
@@ -18,11 +18,13 @@
 
 package org.apache.metron.stellar.dsl.functions;
 
-import org.apache.metron.stellar.dsl.BaseStellarFunction;
-import org.apache.metron.stellar.dsl.Stellar;
-
+import com.google.common.collect.Iterables;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.metron.stellar.dsl.BaseStellarFunction;
+import org.apache.metron.stellar.dsl.Stellar;
 
 public class MapFunctions {
 
@@ -52,15 +54,15 @@ public class MapFunctions {
   }
 
   @Stellar(name="GET"
-          ,namespace="MAP"
-          , description="Gets the value associated with a key from a map"
-          , params = {
-                      "key - The key"
-                     ,"map - The map"
-                     ,"default - Optionally the default value to return if the key is not in the map."
-                     }
-          , returns = "The object associated with the key in the map.  If no value is associated with the key and default is specified, then default is returned. If no value is associated with the key or default, then null is returned."
-          )
+      ,namespace="MAP"
+      , description="Gets the value associated with a key from a map"
+      , params = {
+      "key - The key"
+      ,"map - The map"
+      ,"default - Optionally the default value to return if the key is not in the map."
+  }
+      , returns = "The object associated with the key in the map.  If no value is associated with the key and default is specified, then default is returned. If no value is associated with the key or default, then null is returned."
+  )
   public static class MapGet extends BaseStellarFunction {
     @Override
     @SuppressWarnings("unchecked")
@@ -78,6 +80,73 @@ public class MapFunctions {
       Object ret = map.get(keyObj);
       if(ret == null && defaultObj != null) {
         return defaultObj;
+      }
+      return ret;
+    }
+  }
+
+  @Stellar(name = "PUT",
+           namespace = "MAP",
+           description = "Adds a key/value pair to a map",
+           params = {
+                      "key - The key",
+                      "value - The value",
+                      "map - The map to perform the put on"
+                    },
+           returns = "The original map modified with the key/value. If the map argument is null, a new map will be created and returned that contains the provided key and value - note: if the 'map' argument is null, only the returned map will be non-null and contain the key/value."
+          )
+  public static class MapPut extends BaseStellarFunction {
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Object apply(List<Object> objects) {
+      if (objects.size() < 3) {
+        throw new IllegalArgumentException("Must pass a key, value, and map");
+      } else {
+        Object keyObj = objects.get(0);
+        Object valueObj = objects.get(1);
+        Object mapObj = objects.get(2);
+        if (mapObj == null) {
+          mapObj = new HashMap<>();
+        }
+        Map<Object, Object> map = (Map) mapObj;
+        map.put(keyObj, valueObj);
+        return map;
+      }
+    }
+  }
+
+  @Stellar(name = "MERGE",
+           namespace = "MAP",
+           description = "Merges a list of maps",
+           params = {"maps - A collection of maps to merge. Last entry wins for overlapping keys."},
+           returns = "A Map. null if the list of maps is empty."
+          )
+  public static class MapMerge extends BaseStellarFunction {
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Object apply(List<Object> list) {
+      if (list.size() < 1) {
+        return null;
+      }
+      LinkedHashMap<Object, Object> ret = new LinkedHashMap<>();
+      Object o = list.get(0);
+      if (o != null) {
+        if (!(o instanceof Iterable)) {
+          throw new IllegalArgumentException("Expected an Iterable, but " + o + " is of type " + o.getClass());
+        }
+        Iterable<? extends Map> maps = (Iterable<? extends Map>) o;
+
+        if (Iterables.size(maps) == 1) {
+          return Iterables.getFirst(maps, null);
+        }
+
+        for (Map m : maps) {
+          if (m != null) {
+            ret.putAll(m);
+          }
+        }
       }
       return ret;
     }
