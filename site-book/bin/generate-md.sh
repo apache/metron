d92e0a2102306ce6d6b6b2a8c986590b030d4a88
tigerquoll <tigerquoll@outlook.com>
2019-05-13 16:59:01 -0600

    METRON-2075 Site book build support for MacOS that has GNU sed installed (tigerquoll via mmiklavc) closes apache/metron#1384

--- site-book/bin/generate-md.sh
+++ site-book/bin/generate-md.sh
@@ -264,8 +264,9 @@ for (( i=0; i<${#HREF_REWRITE_LIST[@]} ; i+=2 )) ; do
             sed -i -e "${HREF_REWRITE_LIST[ $(( i + 1 )) ]}" "${HREF_REWRITE_LIST[$i]}"
             ;;
         darwin*)
+	    # Use absolute path to ensure that MacOS sed is being used
             # MacOS sed needs an empty-string argument after -i option to get the same result
-            sed -i '' -e "${HREF_REWRITE_LIST[ $(( i + 1 )) ]}" "${HREF_REWRITE_LIST[$i]}"
+            /usr/bin/sed -i '' -e "${HREF_REWRITE_LIST[ $(( i + 1 )) ]}" "${HREF_REWRITE_LIST[$i]}"
             ;;
         *)
             echo "ERROR: Unable to determine 'sed' argument list for OS ${OSTYPE}" > /dev/stderr
