8ef3f03f3600a4f4cfd0b09b17dc5eeb76d398a1
Nick Allen <nick@nickallen.org>
2019-07-03 12:51:12 -0400

    Introducing new abstracts that work with HBase 2.0.2

---
+++ metron-platform/metron-hbase/metron-hbase-common/src/test/java/org/apache/metron/hbase/client/StaticFakeHBaseConnectionFactory.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.metron.hbase.client;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.metron.hbase.client.HBaseConnectionFactory;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class StaticFakeHBaseConnectionFactory extends HBaseConnectionFactory {
+
+  /**
+   * A set of {@link Table}s that will be returned by all {@link Connection}
+   * objects created by this factory.
+   */
+  private static Map<TableName, Table> tables = new HashMap<>();
+
+  /**
+   * The {@link Connection} returned by this factory will return the given table by
+   * name when {@link Connection#getTable(TableName)} is called.
+   *
+   * @param tableName The name of the table.
+   * @param table The table.
+   * @return
+   */
+  public static void withTable(String tableName, Table table) {
+    tables.put(TableName.valueOf(tableName), table);
+  }
+
+  /**
+   * The {@link Connection} returned by this factory will return a table by
+   * name when {@link Connection#getTable(TableName)} is called.
+   *
+   * @param tableName The name of the table.
+   * @return
+   */
+  public static void withTable(String tableName) {
+    withTable(tableName, mock(Table.class));
+  }
+
+  public static Table getTable(String tableName) {
+    return tables.get(TableName.valueOf(tableName));
+  }
+
+  @Override
+  public Connection createConnection(Configuration configuration) throws IOException {
+    Connection connection = mock(Connection.class);
+
+    // the connection must return each of the given tables by name
+    for(Map.Entry<TableName, Table> entry: tables.entrySet()) {
+      TableName tableName = entry.getKey();
+      Table table = entry.getValue();
+      when(connection.getTable(eq(tableName))).thenReturn(table);
+    }
+
+    return connection;
+  }
+}
