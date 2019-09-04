cd01fc917b03b63074b20c1a0c4ae37f28796432
Tibor Meller <tibor.meller@gmail.com>
2019-09-04 12:02:04 +0200

    METRON-2210 covering scheduled polling

--- metron-interface/metron-alerts/src/app/alerts/alerts-list/auto-polling/auto-polling.service.spec.ts
+++ metron-interface/metron-alerts/src/app/alerts/alerts-list/auto-polling/auto-polling.service.spec.ts
@@ -1,7 +1,24 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { AutoPollingService } from './auto-polling.service';
 import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { SearchService } from 'app/service/search.service';
-import { Subject } from 'rxjs';
+import { Subject, of } from 'rxjs';
 import { QueryBuilder } from '../query-builder';
 import { SearchResponse } from 'app/model/search-response';
 
@@ -9,11 +26,6 @@ fdescribe('AutoPollingService', () => {
 
   let autoPollingService: AutoPollingService;
   let searchService: SearchService;
-  const fakeSearchObservable = new Subject<SearchResponse>();
-
-  function emitInitialResponse() {
-    fakeSearchObservable.next(new SearchResponse());
-  }
 
   function getIntervalInMS(): number {
     return autoPollingService.getInterval() * 1000;
@@ -27,7 +39,7 @@ fdescribe('AutoPollingService', () => {
       providers: [
         AutoPollingService,
         { provide: SearchService, useClass: () => { return {
-          search: () => fakeSearchObservable,
+          search: () => of(new SearchResponse()),
         } } },
         { provide: QueryBuilder, useClass: () => { return {
           addOrUpdateFilter: () => {},
@@ -72,15 +84,12 @@ fdescribe('AutoPollingService', () => {
     });
 
     autoPollingService.start();
-
-    emitInitialResponse();
   });
 
   it('should start polling when start called', fakeAsync(() => {
     spyOn(searchService, 'search').and.callThrough();
 
     autoPollingService.start();
-    emitInitialResponse();
 
     tick(getIntervalInMS());
 
@@ -90,10 +99,13 @@ fdescribe('AutoPollingService', () => {
   }));
 
   it('should broadcast polling response via data subject', fakeAsync(() => {
+    const fakeSearchObservable = new Subject<SearchResponse>();
     const fakePollResponse = new SearchResponse();
 
     autoPollingService.start();
-    emitInitialResponse();
+    // The reason am mocking the searchService.search here is to not interfere
+    // with the initial request triggered right after the start
+    searchService.search = () => fakeSearchObservable;
 
     autoPollingService.data.subscribe((result) => {
       expect(result).toBe(fakePollResponse);
@@ -104,5 +116,41 @@ fdescribe('AutoPollingService', () => {
 
     fakeSearchObservable.next(fakePollResponse);
   }));
+
+  it('should keep polling and broadcasting based on the interval', fakeAsync(() => {
+    const fakeSearchObservable = new Subject<SearchResponse>();
+    const broadcastObserverSpy = jasmine.createSpy('broadcastObserverSpy');
+    const testInterval = 2;
+
+    autoPollingService.setInterval(testInterval);
+    autoPollingService.start();
+
+    // The reason am mocking the searchService.search here is to not interfere
+    // with the initial request triggered right after the start
+    searchService.search = () => fakeSearchObservable;
+    spyOn(searchService, 'search').and.callThrough();
+
+    autoPollingService.data.subscribe(broadcastObserverSpy);
+
+    tick(testInterval * 1000);
+    expect(searchService.search).toHaveBeenCalledTimes(1);
+
+    fakeSearchObservable.next({ total: 2 } as SearchResponse);
+    expect(broadcastObserverSpy).toHaveBeenCalledTimes(1);
+    expect(broadcastObserverSpy.calls.argsFor(0)[0]).toEqual({ total: 2 });
+
+    tick(testInterval * 1000);
+    expect(searchService.search).toHaveBeenCalledTimes(2);
+
+    fakeSearchObservable.next({ total: 3 } as SearchResponse);
+    expect(broadcastObserverSpy).toHaveBeenCalledTimes(2);
+    expect(broadcastObserverSpy.calls.argsFor(1)[0]).toEqual({ total: 3 });
+
+    autoPollingService.stop();
+  }));
+
+  it('should use latest query from query builder', () => {
+
+  });
 
 });
