9da977e6b40078d5cebdfcc556ffbd41f44af1a7
Tibor Meller <tibor.meller@gmail.com>
2019-09-16 16:06:38 +0200

    METRON-2210 Adding cypress test file for auto polling

---
+++ metron-interface/metron-alerts/cypress/integration/auto-polling/auto-polling.feature.spec.js
@@ -0,0 +1,96 @@
+/// <reference types="Cypress" />
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import * as appConfigJSON from '../../../src/assets/app-config.json';
+
+context('Automatic data polling on Alerts View', () => {
+
+  const configuringDefaultStubs = () => {
+    cy.route({
+      method: 'GET',
+      url: '/api/v1/user',
+      response: 'user'
+    });
+
+    cy.route('GET', '/api/v1/global/config', 'fixture:config.json');
+    cy.route('GET', appConfigJSON.contextMenuConfigURL, 'fixture:context-menu.conf.json');
+  };
+
+  beforeEach(() => {
+    cy.server();
+    configuringDefaultStubs();
+  });
+
+  it('auto polling should start by clicking on play icon', () => {
+    cy.visit('login');
+    cy.get('[name="user"]').type('user');
+    cy.get('[name="password"]').type('password');
+    cy.contains('LOG IN').click();
+
+    cy.route({
+      url: '/api/v1/search/search',
+      method: 'POST',
+      onRequest: (req) => {
+        expect(req).to.not.be.undefined;
+      }
+    });
+
+    cy.get('app-auto-polling > .btn').click();
+  });
+
+  it('auto polling should keep polling after start depending on polling interval', () => {
+    cy.visit('login');
+    cy.get('[name="user"]').type('user');
+    cy.get('[name="password"]').type('password');
+    cy.contains('LOG IN').click();
+
+    cy.clock();
+
+    cy.route({
+      url: '/api/v1/search/search',
+      method: 'POST',
+      response: 'fixture:search.json',
+    });
+
+    cy.get('app-auto-polling > .btn').click();
+
+    cy.route({
+      url: '/api/v1/search/search',
+      method: 'POST',
+      response: 'fixture:search.json',
+    }).as('secondPollingRequest');
+
+    cy.wait('@secondPollingRequest').then((req) => {
+      expect(req).to.not.be.undefined;
+    });
+
+    cy.tick(9500);
+
+    cy.route({
+      url: '/api/v1/search/search',
+      method: 'POST',
+      response: 'fixture:search.json',
+    }).as('thirdPollingRequest');
+
+    cy.wait('@thirdPollingRequest').then((req) => {
+      expect(req).to.not.be.undefined;
+    });
+
+    cy.tick(10000);
+  });
+});
