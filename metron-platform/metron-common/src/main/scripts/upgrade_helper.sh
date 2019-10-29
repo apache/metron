220a36175b6825a4d0c1b54ef9ede67ebd2673b5
Michael Miklavcic <michael.miklavcic@gmail.com>
2019-10-28 19:39:55 -0600

    Add detection for Ambari and Metron existence

--- metron-platform/metron-common/src/main/scripts/upgrade_helper.sh
+++ metron-platform/metron-common/src/main/scripts/upgrade_helper.sh
@@ -20,52 +20,79 @@ cluster_name=$5
 source /etc/default/metron
 
 OUT_DIR=metron-backup
-if [ ! -d "$OUT_DIR" ]; then
-    mkdir $OUT_DIR
-fi
 AMBARI_CONFIG_DIR=$OUT_DIR/ambari-configs
-if [ ! -d "$AMBARI_CONFIG_DIR" ]; then
-    mkdir $AMBARI_CONFIG_DIR
-fi
 ZK_CONFIG_DIR=$OUT_DIR/zk-configs
-if [ ! -d "$ZK_CONFIG_DIR" ]; then
-    mkdir $ZK_CONFIG_DIR
-fi
 
-if [ "$mode" == "backup" ];
-then
-    echo Backing up Ambari config...
-    for config_type in $(curl -u $username:$password -H "X-Requested-By: ambari" -X GET  http://$ambari_address/api/v1/clusters/$cluster_name?fields=Clusters/desired_configs | grep '" : {' | grep -v Clusters | grep -v desired_configs | cut -d'"' -f2 | grep metron); 
-    do 
-        echo Saving $config_type
-        /var/lib/ambari-server/resources/scripts/configs.py -u $username -p $password -a get -l ${ambari_address%:*} -n $cluster_name -c $config_type -f $AMBARI_CONFIG_DIR/${config_type}.json
-    done
-    echo Done backing up Ambari config...
+if [ "$mode" == "backup" ]; then
+    if [ ! -d "$OUT_DIR" ]; then
+        mkdir $OUT_DIR
+    fi
+    if [ ! -d "$AMBARI_CONFIG_DIR" ]; then
+        mkdir $AMBARI_CONFIG_DIR
+    fi
+    if [ ! -d "$ZK_CONFIG_DIR" ]; then
+        mkdir $ZK_CONFIG_DIR
+    fi
+    if [ -f "/var/lib/ambari-server/resources/scripts/configs.py" ]; then
+        echo Backing up Ambari config...
+        for config_type in $(curl -u $username:$password -H "X-Requested-By: ambari" -X GET  http://$ambari_address/api/v1/clusters/$cluster_name?fields=Clusters/desired_configs | grep '" : {' | grep -v Clusters | grep -v desired_configs | cut -d'"' -f2 | grep metron); 
+        do 
+            echo Saving $config_type
+            /var/lib/ambari-server/resources/scripts/configs.py -u $username -p $password -a get -l ${ambari_address%:*} -n $cluster_name -c $config_type -f $AMBARI_CONFIG_DIR/${config_type}.json
+        done
+        echo Done backing up Ambari config...
+    else
+        echo Skipping Ambari config backup - Ambari not found on this host
+    fi
 
-    echo Backing up Metron config
-    $METRON_HOME/bin/zk_load_configs.sh -m PULL -o $ZK_CONFIG_DIR -z $ZOOKEEPER -f
-    echo Done backing up Metron config
-elif [ "$mode" == "restore" ];
-then
-    echo Restoring metron config from files in $AMBARI_CONFIG_DIR
-    i=0
-    for filename in $AMBARI_CONFIG_DIR/*;
-    do
-        ((i=i+1))
-        filename=${filename##*/}
-        echo $i. Found config: $filename
-        config_type=${filename%.json}
-        echo "   Setting config_type to $config_type"
-        /var/lib/ambari-server/resources/scripts/configs.py -u $username -p $password -a set -l ${ambari_address%:*} -n $cluster_name -c $config_type -f $AMBARI_CONFIG_DIR/${config_type}.json
-        echo "   Done restoring $config_type"
-    done
-    echo Done restoring $i metron config files from $OUT_DIR
+    if [ -f "$METRON_HOME/bin/zk_load_configs.sh" ]; then
+        echo Backing up Metron config
+        $METRON_HOME/bin/zk_load_configs.sh -m PULL -o $ZK_CONFIG_DIR -z $ZOOKEEPER -f
+        echo Done backing up Metron config
+    else
+        echo Skipping Metron config backup - Metron not found on this host
+    fi
+elif [ "$mode" == "restore" ]; then
+    if [ ! -d "$OUT_DIR" ]; then
+        echo Backup directory not found, aborting
+        exit -1
+    fi
+    if [ -f "/var/lib/ambari-server/resources/scripts/configs.py" ]; then
+        if [ -d "$AMBARI_CONFIG_DIR" ]; then
+            echo Restoring metron config from files in $AMBARI_CONFIG_DIR
+            i=0
+            for filename in $AMBARI_CONFIG_DIR/*;
+            do
+                [ -e "$filename" ] || continue
+                ((i=i+1))
+                filename=${filename##*/}
+                echo $i. Found config: $filename
+                config_type=${filename%.json}
+                echo "   Setting config_type to $config_type"
+                /var/lib/ambari-server/resources/scripts/configs.py -u $username -p $password -a set -l ${ambari_address%:*} -n $cluster_name -c $config_type -f $AMBARI_CONFIG_DIR/${config_type}.json
+                echo "   Done restoring $config_type"
+            done
+            echo Done restoring $i metron config files from $OUT_DIR
+        else
+            echo Ambari backup directory not found, skipping 
+        fi
+    else
+        echo Skipping Ambari config restore - Ambari not found on this host
+    fi
 
-    echo Restoring Metron zookeeper config from files in $ZK_CONFIG_DIR
-    $METRON_HOME/bin/zk_load_configs.sh -m PUSH -i $ZK_CONFIG_DIR -z $ZOOKEEPER -f
-    echo Pulling config locally into Metron home config dir
-    $METRON_HOME/bin/zk_load_configs.sh -m PULL -o ${METRON_HOME}/config/zookeeper -z $ZOOKEEPER -f
-    echo Done restoring Metron zookeeper config from files in $ZK_CONFIG_DIR
+    if [ -f "$METRON_HOME/bin/zk_load_configs.sh" ]; then
+        if [ -d "$ZK_CONFIG_DIR" ]; then
+            echo Restoring Metron zookeeper config from files in $ZK_CONFIG_DIR
+            $METRON_HOME/bin/zk_load_configs.sh -m PUSH -i $ZK_CONFIG_DIR -z $ZOOKEEPER -f
+            echo Pulling config locally into Metron home config dir
+            $METRON_HOME/bin/zk_load_configs.sh -m PULL -o ${METRON_HOME}/config/zookeeper -z $ZOOKEEPER -f
+            echo Done restoring Metron zookeeper config from files in $ZK_CONFIG_DIR
+        else
+            echo Metron config backup directory not found, skipping 
+        fi
+    else
+        echo Skipping Metron config restore - Metron not found on this host
+    fi
 else
     echo Mode \"$mode\" not recognized. Exiting.
 fi
